8c9c5569c7fbbbc9e94cdfae9dfa92d9d7ee2df7
YONGHYUK LEE <yonghyuklee@YONGHYUKui-MacBook.local>
2019-04-21 11:20:22 +0900

    eslint config files

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-boolean-value":"off",
+        "import/no-unresolved": "off",
+        "import/extensions": "off",
+        "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+        "jsx-a11y/anchor-is-valid": [
+            "error",
+            {
+                "components": ["Link"],
+                "specialLink": ["to", "hrefLeft", "hrefRight"],
+                "aspects": ["noHref", "invalidHref", "preferButton"],
+            },
+        ],
+        "linebreak-style": 0,
+        "react/forbid-prop-types": [1, { "forbid": ['any'], checkContextTypes: true, checkChildContextTypes: true }],
+        "max-len": ["warn", { "code": 150 }],
+    }
+};
