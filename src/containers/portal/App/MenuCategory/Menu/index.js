0cbbacecc9e24229456fc5fd2e428504c0a140b4
sylee <6831325a@gmail.com>
2019-07-19 19:36:40 +0900

    portal  메뉴 수정

--- src/containers/portal/App/MenuCategory/Menu/index.js
+++ src/containers/portal/App/MenuCategory/Menu/index.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SideMenu from 'react-sidemenu';
 
 import StlyedDefaultTheme from './StlyedDefaultTheme';
 
-const Tree = ({ treeData, handleClick }) => (
-  <StlyedDefaultTheme>
-    <SideMenu collapse={true} items={treeData} onMenuItemClick={handleClick} />
-  </StlyedDefaultTheme>
-);
+class Tree extends Component {
+  /* eslint-disable */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeItem: '',
+    };
+  }
+
+  handleClick = (value, extas) => {
+    const { execMenu, execPage } = this.props;
+
+    this.setState({
+      activeItem: value.toString(),
+    });
+
+    if (extas.TARGET === 'NEW') {
+      window.open(extas.URL, extas.MENU_ID, 'width=1280, height=720, toolbar=yes, resizable=yes, menubar=yes, status=yes, location=yes');
+      execMenu(extas.PAGE_ID, extas.TARGET);
+    } else if (extas.NODE_TYPE !== 'F' && extas.APP_YN !== 'F') {
+      execPage(extas, 'execMenu');
+    }
+  };
+
+  render() {
+    const { treeData } = this.props;
+
+    return (
+      <StlyedDefaultTheme>
+        <SideMenu collapse={true} items={treeData} activeItem={this.state.activeItem} onMenuItemClick={this.handleClick} />
+      </StlyedDefaultTheme>
+    );
+  }
+}
 
 Tree.propTypes = {
   treeData: PropTypes.array,
-  handleClick: PropTypes.func,
+  execMenu: PropTypes.func,
+  execPage: PropTypes.func,
 };
 
 Tree.defaultProps = {
   treeData: [],
-  handleClick: () => {},
+  execMenu: () => {},
+  execPage: () => {},
 };
 
 export default Tree;
