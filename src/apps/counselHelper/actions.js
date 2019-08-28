c786d31294e34be2a57f76dff9891b24c4c0a079
JungHoe <51188350+JungHoe@users.noreply.github.com>
2019-08-28 19:01:41 +0900

    redux 수정
    ^~^

--- src/apps/counselHelper/actions.js
+++ src/apps/counselHelper/actions.js
@@ -1,11 +1,13 @@
 import * as action from './constants';
 
-export const saveDetail = detail => ({
+export const saveDetail = (detail, WIDGET_ID) => ({
   type: action.SAVE_DETAIL,
   detail,
+  WIDGET_ID,
 });
 
-export const getDetail = detail => ({
+export const getDetail = (detail, WIDGET_ID) => ({
   type: action.GET_DETAIL,
   detail,
+  WIDGET_ID,
 });
--- src/apps/counselHelper/selectors.js
+++ src/apps/counselHelper/selectors.js
@@ -5,7 +5,8 @@ const selectAppsWidgetState = state => state.get('apps-Widget');
 const makeSelectDetail = () =>
   createSelector(
     selectAppsWidgetState,
-    widgetstate => widgetstate.get('detail'),
+    (widgetstate, props) => (props && props.item && props.item.WIDGET_ID ? props.item.WIDGET_ID : 11052),
+    (widgetstate, WIDGET_ID) => widgetstate.getIn(['detailMap', WIDGET_ID, 'detail']),
   );
 
 export default { selectAppsWidgetState, makeSelectDetail };
