e993e462b5fa6348efdb3fe3aa21069ae512f419
jdlee <jdlee@kb-sys.co.kr>
2019-09-04 20:41:05 +0900

    admin menu

---
+++ src/containers/admin/AdminMain/LeftMenu/MenuItem/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from 'antd';
+import { /* intlObj, */ lang } from 'utils/commonUtils';
+
+class MenuItem extends Component {
+  handleClick = item => {
+    const { setOpenMenuCode } = this.props;
+    if (!item.child) this.props.classChange(item.URL);
+    else {
+      setOpenMenuCode(item.SCR_CD);
+    }
+  };
+
+  subMenuOpen = item => {
+    const { openMenuCode } = this.props;
+    return item.child && openMenuCode === item.SCR_CD;
+  };
+
+  render() {
+    const { menuItem, classString, classChange, setIcon } = this.props;
+
+    return (
+      <li className={`${classString(menuItem.URL)}${this.subMenuOpen(menuItem) ? ' open' : ''}`}>
+        <a onClick={() => this.handleClick(menuItem)} onKeyPress={() => this.handleClick(menuItem)}>
+          <Icon type={setIcon(menuItem.SCR_CD)} />
+          <span className="nav-link-text">{lang.get('NAME', menuItem)}</span>
+          {menuItem.child && (
+            <b className="collapse-sign">
+              <em className={`fa${this.subMenuOpen(menuItem) ? ' fa-angle-down' : ' fa-angle-up'}`}></em>
+            </b>
+          )}
+        </a>
+        {menuItem.child && (
+          <ul>
+            {menuItem.child.map(s => (
+              <li className={classString(s.URL)}>
+                <a onClick={() => classChange(s.URL)} onKeyPress={() => classChange(s.URL)}>
+                  <span className="nav-link-text">{lang.get('NAME', s)}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </li>
+    );
+  }
+}
+
+MenuItem.propTypes = {
+  menuItem: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+  classString: PropTypes.func.isRequired,
+  classChange: PropTypes.func.isRequired,
+  setIcon: PropTypes.func.isRequired,
+  setOpenMenuCode: PropTypes.func.isRequired,
+  openMenuCode: PropTypes.string.isRequired,
+};
+
+export default MenuItem;
