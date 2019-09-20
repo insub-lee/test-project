2f74ecfbac263514f0c580ce391f39a0ad21897c
이정회 <jhlee2@kb-sys.co.kr>
2019-09-20 18:07:04 +0900

    update csview

--- src/apps/manual/user/CSManualView/ExplainInfo/Tab/index.js
+++ src/apps/manual/user/CSManualView/ExplainInfo/Tab/index.js
@@ -2,24 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tab, Tabs as ReactTabs, TabList, TabPanel } from 'react-tabs';
 
+import StyledWriteTabPanel from '../../../../components/WritePage/StyledWriteTabPanel';
+import FroalaEditorView from '../../../../components/RichTextEditor/FroalaEditorView';
 import Styled from './Styled';
-
-const Tabs = ({ tabs, keyName, selectedTabIdx, setSelectedTabIdx, widgetId }) => (
-  <Styled>
-    <ReactTabs selectedIndex={selectedTabIdx} onSelect={tabIndex => setSelectedTabIdx(tabIndex, widgetId)}>
-      <TabList>
-        {tabs.map(tab => (
-          <Tab key={tab[keyName]} disabled={tab.disabled}>
-            {tab.MUAL_TABNAME}
-          </Tab>
+const getlength = data => data.length;
+const Tabs = ({ tabData, keyName }) => {
+  const view = tabData.map(data => ({
+    id: data.id,
+    TabComponent: <div className="titleWrap">{data.title}</div>,
+    TabPanelComponent: (
+      <StyledWriteTabPanel>
+        <FroalaEditorView model={data.board} />
+      </StyledWriteTabPanel>
+    ),
+    disabled: false,
+  }));
+  const length = getlength(view);
+  return (
+    <Styled length={length}>
+      <ReactTabs>
+        <TabList>
+          {view.map(tab => (
+            <Tab key={tab[keyName]} disabled={tab.disabled}>
+              {tab.TabComponent}
+            </Tab>
+          ))}
+        </TabList>
+        {view.map(tab => (
+          <TabPanel key={tab[keyName]}>{tab.TabPanelComponent}</TabPanel>
         ))}
-      </TabList>
-      {tabs.map(tab => (
-        <TabPanel key={tab[keyName]}>{tab.TabPanelComponent}</TabPanel>
-      ))}
-    </ReactTabs>
-  </Styled>
-);
+      </ReactTabs>
+    </Styled>
+  );
+};
 
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.object),
