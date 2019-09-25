91b36c33d7aa011628946e52e6a88f680a2ecb5f
cornpie <no1toldemewhy@gmail.com>
2019-09-25 18:56:38 +0900

    FORMSTUFF

--- src/components/FormStuff/Checkbox/Wrapper.js
+++ src/components/FormStuff/Checkbox/Wrapper.js
@@ -1,6 +1,40 @@
 import styled from 'styled-components';
 
-const CheckWrapper = ComponentName => styled(ComponentName)``;
+const CheckWrapper = ComponentName => styled(ComponentName)`
+  & .ant-checkbox {
+    & .ant-checkbox-inner {
+      width: 1.125rem;
+      height: 1.125rem;
+      border: #adb5bd solid 2px;
+    }
+    &.ant-checkbox-checked {
+      &:after {
+        border: 1px solid #6e4e9e;
+      }
+      & .ant-checkbox-inner {
+        background-color: #7a59ad;
+        border-color: #6e4e9e;
+        &:after {
+          background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+          background-size: 50% 50%;
+          position: absolute;
+          display: block;
+          width: 1.125rem;
+          height: 1.125rem;
+          content: '';
+          background-repeat: no-repeat;
+          background-position: 0.125rem 0.125rem;
+          top: initial;
+          left: initial;
+          border: none;
+          -webkit-transform: none;
+          -ms-transform: none;
+          transform: none;
+        }
+      }
+    }
+  }
+`;
 const CheckBoxGroupWrapper = ComponentName => styled(ComponentName)``;
 
 export { CheckWrapper, CheckBoxGroupWrapper };
--- src/components/FormStuff/Radio/Wrapper.js
+++ src/components/FormStuff/Radio/Wrapper.js
@@ -1,6 +1,41 @@
 import styled from 'styled-components';
 
-const RadioWrapper = ComponentName => styled(ComponentName)``;
+const RadioWrapper = ComponentName => styled(ComponentName)`
+  & .ant-radio {
+    & .ant-radio-inner {
+      width: 1.125rem;
+      height: 1.125rem;
+      border: #adb5bd solid 2px;
+    }
+    &.ant-radio-checked {
+      &:after {
+        border: 1px solid #6e4e9e;
+      }
+      & .ant-radio-inner {
+        background-color: #7a59ad;
+        border-color: #6e4e9e;
+        &:after {
+          width: 8px;
+          height: 8px;
+          top: 3px;
+          left: 3px;
+          background-color: #7a59ad;
+          background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e");
+          position: absolute;
+          display: block;
+          content: '';
+          background-repeat: no-repeat;
+          border: none;
+          -webkit-transform: none;
+          -ms-transform: none;
+          -webkit-transform: none;
+          -ms-transform: none;
+          transform: none;
+        }
+      }
+    }
+  }
+`;
 const RadioGroupWrapper = ComponentName => styled(ComponentName)``;
 
 export { RadioWrapper, RadioGroupWrapper };
