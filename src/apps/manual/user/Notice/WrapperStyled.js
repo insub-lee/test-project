30cf3cf7e8d152fcb53201d18e1fc55440d08def
이정회 <jhlee2@kb-sys.co.kr>
2019-10-06 17:23:40 +0900

    CSS Update

---
+++ src/apps/manual/user/Notice/WrapperStyled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: #fff;
+  border: 1px solid #eaeaea;
+  padding: 40px;
+  border-radius: 3px;
+  .categorie {
+    padding-right: 15px;
+  }
+  .builderBaseData {
+    border-left: 1px solid #e5e5e5;
+    padding-left: 15px;
+    .ant-table-thead {
+      th {
+        background: #886ab5;
+        color: #fff;
+      }
+    }
+  }
+`;
+
+export default Wrapper;
