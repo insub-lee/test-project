fa2db90e4c8faaab985d423eef0a5f9ccca81c35
이정회 <jhlee2@kb-sys.co.kr>
2019-11-07 10:29:07 +0900

    ProcessMgr Admin 메뉴등록

--- src/containers/admin/AdminMain/index.js
+++ src/containers/admin/AdminMain/index.js
@@ -9,6 +9,7 @@ import WorkBuilderListPage from 'apps/WorkBuilderApp/Admin/WorkBuilderListPage';
 import WorkBuilderToAppPage from 'apps/WorkBuilderApp/Admin/WorkBuilderToAppPage';
 import FileManager from 'apps/FileManager';
 import ViewDesigner from 'apps/mdcs/admin/ViewDesigner';
+import ProcessMgr from 'apps/WorkFlow/Admin/ProcessMgr';
 
 import CodeAdmin from './CodeAdmin';
 import SiteAdmin from './SiteAdmin';
@@ -72,6 +73,7 @@ class wrap extends PureComponent {
           <Route path="/admin/adminmain/classify" component={Classify} />
           <Route path="/admin/adminmain/node" component={Node} />
           <Route path="/admin/adminmain/viewdesigner" component={ViewDesigner} />
+          <Route path="/admin/adminmain/ProcessMgr" component={ProcessMgr} />
           {/* Route 추가시 아래의 Route 위에 추가 하세요 */}
           <Route path="/admin/adminmain/:MENU/" component={Menu} />
         </Switch>
