cbf2ff7940c2612aca95e267b4ff53218be1f77d
jhjeong <jhjeong@kb-sys.co.kr>
2019-12-20 20:32:42 +0900

    Add - no console function

--- src/include/index.js
+++ src/include/index.js
@@ -6,3 +6,12 @@ import 'rodal/lib/rodal.css';
 
 window.jQuery = $;
 window.$ = $;
+
+/* hide console when use production level */
+function noConsole() {}
+if (process.env.NODE_ENV === 'production') {
+  console.log = noConsole;
+  console.error = noConsole;
+  console.warn = noConsole;
+  console.debug = noConsole;
+}
