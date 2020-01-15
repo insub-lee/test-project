1dc2c784e3b4cea9961c320411ddec5b54efabbb
김준혁 <jhkim3@kb-sys.co.kr>
2020-01-16 02:05:55 +0900

    fmea 표시유무 수정

--- src/components/BizBuilder/Field/FmeaTextareaComp.js
+++ src/components/BizBuilder/Field/FmeaTextareaComp.js
@@ -11,6 +11,20 @@ class FmeaTextareaComp extends Component {
     }
   };
 
+  componentDidUpdate = prevProps => {
+    const { isManage, formData, rowClass } = this.props;
+    const { formData: prevFormData } = prevProps;
+    if (formData.FMEA_FLAG && prevFormData.FMEA_FLAG && formData.FMEA_FLAG !== prevFormData.FMEA_FLAG) {
+      if (!isManage && rowClass && Number(formData.FMEA_FLAG) !== 195) {
+        const rowNode = document.querySelector(`.${rowClass}`);
+        rowNode.style.display = 'none';
+      } else if (rowClass) {
+        const rowNode = document.querySelector(`.${rowClass}`);
+        rowNode.style.display = '';
+      }
+    }
+  };
+
   render = () => {
     const { isManage, formData, visible } = this.props;
     return isManage || (visible && Number(formData.FMEA_FLAG) === 195) ? <TextareaComp {...this.props} /> : '';
