59c202a86342d7b7b446269b422c53c3f123aa86
김준혁 <jhkim3@kb-sys.co.kr>
2020-01-16 01:45:48 +0900

    search bug fix

--- src/apps/mdcs/user/Search/SearchDetail/Search.js
+++ src/apps/mdcs/user/Search/SearchDetail/Search.js
@@ -33,7 +33,7 @@ const columns = [
     },
   },
   { title: 'No.', key: 'id', dataIndex: 'id' },
-  { title: 'REV.', key: 'status', dataIndex: 'status' },
+  { title: 'REV.', key: 'VERSION', dataIndex: 'VERSION' },
   { title: 'Effect Date', key: 'END_DTTM', dataIndex: 'END_DTTM' },
   { title: 'Title', key: 'title', dataIndex: 'title' },
   { title: '기안부서', key: 'deptName', dataIndex: 'deptName' },
@@ -42,7 +42,7 @@ const columns = [
 
 const initState = {
   searchParam: {
-    nodeId: { key: 'TBD.NODE_ID', condition: '=', value: null, type: 'INT' },
+    nodeId: { key: 'TBD.NODE_ID', condition: 'IN', value: null, type: 'INT' },
     status: { key: 'TBD.STATUS', condition: 'IN', value: 2, type: 'INT' }, // 현재 Revision, 폐기
     docNumber: { key: 'TBD.DOCNUMBER', condition: 'LIKE', value: '', type: 'STRING' },
     title: { key: 'TBD.TITLE', condition: 'LIKE', value: '', type: 'STRING' },
