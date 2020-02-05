da805e528b8290d87979975b435c9b06e634d615
Administrator <admin@example.com>
2020-02-05 21:54:16 +0900

    [Bizmicro-portal-front] add - add some .env for production

--- src/components/Loadable/index.js
+++ src/components/Loadable/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import loadable from 'react-loadable';
+import loadable from 'react-loadable-visibility/loadable-components';
 import Icon from 'antd/lib/icon';
 import Spin from 'antd/lib/spin';
-// import loadable from '@loadable/component';
 // import LoadingIndicator from 'components/LoadingIndicator';
 
 // const Loader = () => <div />;
 
 const indicator = <Icon type="loading" spin style={{ fontSize: 24 }} />;
-const Loader = () => <Spin indicator={indicator} style={{ margin: 'auto', width: '100%', padding: '20%' }} />;
+const Loading = () => <Spin indicator={indicator} style={{ margin: 'auto', width: '100%', padding: '20%' }} />;
 
 /* When use @loadable/component (but loading is not working) */
-// export default ({ loader, ...otherProps }) => loadable(loader, { loading: Loader });
+export default ({ loader, ...otherProps }) => loadable(loader, { fallback: <Loading /> });
 
 /* When use react-loadable */
-export default ({ loader, ...otherProps }) => loadable({ loader, loading: Loader });
+// export default ({ loader, ...otherProps }) => loadable({ loader, loading: Loading });
