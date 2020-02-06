0ce1d9f829a7343293c4cb3bcf20a5057641c13f
hsso <hsso@kb-sys.co.kr>
2020-02-06 16:37:31 +0900

    사용자 관리 기능 완성

--- src/apps/eshs/user/userMgr/List/index.js
+++ src/apps/eshs/user/userMgr/List/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Table, Column } from 'react-virtualized';
 import { Select, Input } from 'antd';
+import debounce from 'lodash/debounce';
 
-const { Search } = Input;
 const { Option } = Select;
+const { Search } = Input;
+const InputGroup = Input.Group;
 
 class List extends Component {
   constructor(props) {
@@ -19,7 +21,10 @@ class List extends Component {
       filteredUserList: [],
       filteredDeptList: [],
       isSelected: false,
+      searchType: 'name_kor',
+      searchValue: '',
     };
+    this.handleFindData = debounce(this.handleFindData, 500);
   }
 
   handleAppStart = () => {
@@ -30,7 +35,7 @@ class List extends Component {
         deptList: deptList.dept.filter(item => item.prnt_id !== 900),
         userList: userList.users,
         filteredUserList: userList.users,
-        filteredDeptList: deptList.dept.filter(item => item.prnt_id !== 900),
+        // filteredDeptList: deptList.dept.filter(item => item.prnt_id !== 900),
       });
     }
   };
@@ -45,13 +50,11 @@ class List extends Component {
     if (e !== 'ZZ') {
       if (hqId) {
         if (!deptId) {
-          console.debug('지역 있고 본부 있고 부서 없음');
           this.setState({
             filteredUserList: userList.filter(item => item.b_area_code === e && item.hq_id === hqId),
             bAreaCode: e,
           });
         } else {
-          console.debug('지역 있고 본부 있고 부서 있음');
           this.setState({
             filteredUserList: userList.filter(item => item.b_area_code === e && item.hq_id === hqId && item.dept_id === deptId),
             bAreaCode: e,
@@ -65,20 +68,17 @@ class List extends Component {
       }
     } else if (hqId) {
       if (!deptId) {
-        console.debug('지역 없고 본부 있고 부서 없음');
         this.setState({
           filteredUserList: userList.filter(item => item.b_area_code === e && item.hq_id === hqId),
           bAreaCode: '',
         });
       } else {
-        console.debug('지역 없고 본부 있고 부서 있음');
         this.setState({
           filteredUserList: userList.filter(item => item.b_area_code === e && item.hq_id === hqId && item.dept_id === deptId),
           bAreaCode: '',
         });
       }
     } else {
-      console.debug('@@@지역으로 초기화@@@');
       this.setState({
         bAreaCode: '',
         filteredUserList: userList,
@@ -87,55 +87,53 @@ class List extends Component {
   };
 
   handleHqOnChange = e => {
-    const { bAreaCode, userList, isSelected } = this.state;
+    const { bAreaCode, userList, deptList } = this.state;
     if (e === 900) {
       if (bAreaCode) {
-        console.debug('@@@1@@@');
         this.setState({
           hqId: 0,
           deptId: 0,
           filteredUserList: userList.filter(item => item.b_area_code === bAreaCode),
+          isSelected: false,
         });
       } else {
-        console.debug('@@@2@@@ 본부로 초기화');
         this.setState({
           hqId: 0,
           deptId: 0,
           filteredUserList: userList,
+          isSelected: false,
         });
       }
       return;
     }
     if (e !== 900) {
       if (bAreaCode) {
-        console.debug('@@@3@@@');
         this.setState({
           hqId: e,
           isSelected: true,
           filteredUserList: userList.filter(item => item.b_area_code === bAreaCode && item.hq_id === e),
+          filteredDeptList: deptList.filter(item => item.prnt_id === e),
         });
       } else {
-        console.debug('@@@4@@@');
         this.setState({
           hqId: e,
           isSelected: true,
           filteredUserList: userList.filter(item => item.hq_id === e),
+          filteredDeptList: deptList.filter(item => item.prnt_id === e),
         });
       }
     }
   };
 
   handleDeptOnChange = e => {
-    const { hqId, bAreaCode, userList, deptId } = this.state;
+    const { hqId, bAreaCode, userList } = this.state;
     if (e === 9999) {
       if (bAreaCode) {
-        console.debug('@@@11@@@');
         this.setState({
           deptId: 0,
-          filteredUserList: userList.filter(item => item.b_area_code === bAreaCode && item.hq_id === e),
+          filteredUserList: userList.filter(item => item.b_area_code === bAreaCode && item.hq_id === hqId),
         });
       } else {
-        console.debug('@@@22@@@');
         this.setState({
           deptId: 0,
           filteredUserList: userList.filter(item => item.hq_id === hqId),
@@ -145,47 +143,120 @@ class List extends Component {
     }
     if (e !== 9999) {
       if (bAreaCode) {
-        console.debug('@@@33@@@');
         this.setState({
           deptId: e,
           filteredUserList: userList.filter(item => item.b_area_code === bAreaCode && item.hq_id === hqId && item.dept_id === e),
         });
       } else {
-        console.debug('@@@44@@@', hqId);
         this.setState({
           deptId: e,
           filteredUserList: userList.filter(item => item.hq_id === hqId && item.dept_id === e),
         });
       }
     }
-    // if (bAreaCode) {
-    //   this.setState({
-    //     deptId: e,
-    //     filteredUserList: filteredUserList.filter(item => item.b_area_code === bAreaCode && item.hq_id === hqId && item.dept_id === e),
-    //   });
-    // } else {
-    //   this.setState({
-    //     deptId: e,
-    //     filteredUserList: filteredUserList.filter(item => item.hq_id === hqId && item.dept_id === e),
-    //   });
-    // }
-    // if (e === 9999) {
-    //   if (bAreaCode) {
-    //     this.setState({
-    //       deptId: 0,
-    //       filteredUserList: filteredUserList.filter(item => item.b_area_code === bAreaCode && item.hq_id === hqId),
-    //     });
-    //   } else {
-    //     this.setState({
-    //       deptId: 0,
-    //       filteredUserList: filteredUserList.filter(item => item.hq_id === hqId),
-    //     });
-    //   }
-    // }
+  };
+
+  handleSearchTypeOnChange = e => {
+    this.setState({
+      searchType: e,
+    });
+  };
+
+  handleOnChange = e => {
+    this.setState({
+      searchValue: e.target.value,
+    });
+    this.handleFindData();
+  };
+
+  handleOnCallBack = () => {
+    const { hqId, deptId, bAreaCode, filteredUserList, searchValue, userList } = this.state;
+    const { result } = this.props;
+    this.setState({
+      filteredUserList: result.searchUser.searchResult,
+    });
+
+    if (!searchValue) {
+      if ((!bAreaCode && !hqId) || (bAreaCode === 'ZZ' && hqId === 900)) {
+        console.debug('@@아무 것도 없고@@');
+        this.setState({
+          filteredUserList: userList,
+        });
+      }
+      // if (bAreaCode && !hqId) {
+      //   console.debug('@@지역만 있고@@');
+      //   this.setState({
+      //     filteredUserList: userList.filter(item => item.b_area_code === bAreaCode),
+      //   });
+      // }
+      // if(!bAreaCode && hqId) {
+      //   if(deptId ) {
+
+      //   }
+      // }
+    }
+    if (!bAreaCode || bAreaCode === 'ZZ') {
+      if (hqId && hqId !== 900) {
+        if (deptId && deptId !== 9999) {
+          console.debug('@@지역 없고 본부 있고 부서 있고 검색어 있고@@');
+          this.setState({
+            filteredUserList: result.searchUser.searchResult.filter(item => item.hq_id === hqId && item.dept_id === deptId),
+          });
+        } else if (!deptId || deptId === 9999) {
+          console.debug('@@지역 없고 본부 있고 부서 없고 검색어 있고@@');
+          this.setState({
+            filteredUserList: result.searchUser.searchResult.filter(item => item.hq_id === hqId),
+          });
+        }
+        return;
+      }
+      if (!hqId || hqId === 900) {
+        console.debug('@@지역 없고 본부 없고 검색어 있고@@');
+        this.setState({
+          filteredUserList: result.searchUser.searchResult,
+        });
+      }
+    }
+    if (bAreaCode && bAreaCode !== 'ZZ') {
+      console.debug('@@지역 있음@@');
+      if (hqId && hqId !== 900) {
+        if (deptId && deptId !== 9999) {
+          console.debug('@@지역 있고 본부 있고 부서 있고 검색어 있고@@');
+          this.setState({
+            filteredUserList: result.searchUser.searchResult.filter(item => item.b_area_code === bAreaCode && item.hq_id === hqId && item.dept_id === deptId),
+          });
+        } else if (!deptId || deptId === 9999) {
+          console.debug('@@지역 있고 본부 있고 부서 없고 검색어 있고@@');
+          this.setState({
+            filteredUserList: result.searchUser.searchResult.filter(item => item.b_area_code === bAreaCode && item.hq_id === hqId),
+          });
+        }
+        return;
+      }
+      if (!hqId || hqId === 900) {
+        console.debug('@@지역 있고 본부 없고 검색어 있고@@');
+        this.setState({
+          filteredUserList: result.searchUser.searchResult.filter(item => item.b_area_code === bAreaCode),
+        });
+      }
+    }
+  };
+
+  handleFindData = () => {
+    const { id, getCallDataHanlder, apiAry } = this.props;
+    const { searchType, searchValue } = this.state;
+
+    const api = {
+      key: 'searchUser',
+      type: 'GET',
+      url: `/api/eshs/v1/common/EshsUserSearch?searchType=${searchType}&keyword=%25${searchValue}%25`,
+    };
+    apiAry.push(api);
+    getCallDataHanlder(id, apiAry, this.handleOnCallBack);
   };
 
   render() {
-    const { isSelected, hqList, deptList, filteredUserList } = this.state;
+    const { isSelected, hqList, filteredDeptList, filteredUserList, searchValue } = this.state;
     return (
       <div>
         <Select defaultValue="지역 전체" style={{ width: 110, padding: 3 }} onChange={this.handleBaseareaChange}>
@@ -201,19 +272,22 @@ class List extends Component {
         </Select>
         <Select defaultValue="팀 전체" style={{ width: 170, padding: 3 }} disabled={!isSelected} onChange={this.handleDeptOnChange}>
           <Option value={9999}>팀 전체</Option>
-          {deptList
-            .filter(item => item.prnt_id === this.state.hqId)
-            .map(item => (
-              <Option value={item.dept_id}>{item.name_kor}</Option>
-            ))}
+          {filteredDeptList.map(item => (
+            <Option value={item.dept_id}>{item.name_kor}</Option>
+          ))}
         </Select>
-        <Search style={{ width: 200, padding: 3 }} />
+        <InputGroup style={{ width: 500, display: 'inline' }} compact>
+          <Select defaultValue="이름" onChange={this.handleSearchTypeOnChange}>
+            <Option value="name_kor">이름</Option>
+            <Option value="emp_no">사번</Option>
+          </Select>
+          <Search placeholder=" 검색어를 입력하세요" style={{ width: 300, padding: 3 }} onChange={this.handleOnChange} value={searchValue} />
+        </InputGroup>
 
         <Table
           width={1100}
           height={1000}
           headerHeight={30}
-          // headerStyle={{ textAlign: 'center' }}
           rowHeight={50}
           rowCount={filteredUserList.length}
           rowGetter={({ index }) => filteredUserList[index]}
@@ -251,16 +325,10 @@ List.defaultProps = {
       params: {},
     },
     {
-      key: 'hqList',
-      type: 'GET',
-      url: `/api/eshs/v1/common/EshsHqAndDeptList?key=900`,
-      // param: { key: 900 },
-    },
-    {
       key: 'deptList',
       type: 'GET',
       url: `/api/eshs/v1/common/EshsHqAndDeptList?key=900`,
-      // param: { key: 900 },
+      // param: { key: '900' },
     },
   ],
 };
