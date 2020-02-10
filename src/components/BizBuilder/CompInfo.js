e62249b328476e53f253007d09c0311cd314f5c6
hyoseong3291 <wjdgytjd3291@gmail.com>
2020-02-10 16:09:51 +0900

    법규 마스터 app 개발중..(검색 및 모달창 요함)

--- src/components/BizBuilder/CompInfo.js
+++ src/components/BizBuilder/CompInfo.js
@@ -46,6 +46,7 @@ import SelectIntComp from 'components/BizBuilder/Field/SelectIntComp';
 import RadioMaterialComp from 'components/BizBuilder/Field/RadioMaterialComp';
 import RadioMaterialLabelComp from 'components/BizBuilder/Field/RadioMaterialLabelComp';
 import RadioPopupComp from 'components/BizBuilder/Field/RadioPopupComp';
+import ManageCodeComp from 'components/BizBuilder/Field/ManageCodeComp';
 
 export const CompInfo = {
   'components/BizBuilder/Field/LabelComp': { renderer: property => <LabelComp {...property} /> },
@@ -94,4 +95,5 @@ export const CompInfo = {
   'components/BizBuilder/Field/RadioMaterialComp': { renderer: property => <RadioMaterialComp {...property} /> },
   'components/BizBuilder/Field/RadioMaterialLabelComp': { renderer: property => <RadioMaterialLabelComp {...property} /> },
   'components/BizBuilder/Field/RadioPopupComp': { renderer: property => <RadioPopupComp {...property} /> },
+  'components/BizBuilder/Field/ManageCodeComp': { renderer: property => <ManageCodeComp {...property} /> },
 };
