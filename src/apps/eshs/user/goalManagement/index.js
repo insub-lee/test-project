7e6e823726deac00b14f30af815959fbdb35b2a9
hsso <hsso@kb-sys.co.kr>
2020-02-11 19:47:47 +0900

    목표관리, ESH 협의체 준비

---
+++ src/apps/eshs/user/goalManagement/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import BizBuilderBase from 'components/BizBuilderBase';
+
+class Notice extends Component {
+  state = {
+    isLoading: true,
+  };
+
+  componentDidMount() {}
+
+  loadingComplete = () => {
+    this.setState({
+      isLoading: false,
+    });
+  };
+
+  render() {
+    return <BizBuilderBase sagaKey="GoalManagement" workSeq={1221} viewType="LIST" loadingComplete={this.loadingComplete} />;
+  }
+}
+
+Notice.propTypes = {};
+
+Notice.defaultProps = {};
+
+export default Notice;
