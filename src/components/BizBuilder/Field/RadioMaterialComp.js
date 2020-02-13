eb2f5c5bd818a9f50725f74e9a32491757d9bea3
lesm74 <sangmin.biz@gmail.com>
2020-02-13 12:53:14 +0900

    Radio Meterial 수정 중

--- src/components/BizBuilder/Field/RadioMaterialComp.js
+++ src/components/BizBuilder/Field/RadioMaterialComp.js
@@ -7,8 +7,14 @@ import LabelComp from './LabelComp';
 const { Option } = Select;
 
 class RadioMaterialComp extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeHandlerText = debounce(this.onChangeHandlerText, 300);
+  }
+
   state = {
     mList: [],
+    isMeterialView: true,
   };
 
   componentDidMount() {
@@ -17,16 +23,37 @@ class RadioMaterialComp extends Component {
   }
 
   initDataBind = id => {
-    console.debug('init', this.props);
     const {
       extraApiData: { meterialList },
     } = this.props;
-    const tempMatList = meterialList.categoryMapList.filter(f => f.LVL > 0).map(item => <Option key={item.NODE_ID}>{item.NAME_KOR}</Option>);
+    const tempMatList = meterialList.categoryMapList
+      .filter(f => f.LVL > 0)
+      .map(item => (
+        <Option key={item.NODE_ID}>
+          {item.CODE}({item.NAME_KOR})
+        </Option>
+      ));
     this.setState({ mList: tempMatList });
   };
 
-  onChangeHandler = value => {
-    console.debug(value);
+  onChangeHandler = e => {
+    const { changeFormData, sagaKey, COMP_FIELD } = this.props;
+    changeFormData(sagaKey, COMP_FIELD, e.target.value);
+    if (e.target.value === 'N') {
+      changeFormData(sagaKey, 'MATERIAL_TYPE', ' ');
+      changeFormData(sagaKey, 'MATERIAL_TEXT', ' ');
+    }
+    this.setState({ isMeterialView: e.target.value === 'Y' });
+  };
+
+  onSelectChange = value => {
+    const { changeFormData, sagaKey } = this.props;
+    changeFormData(sagaKey, 'MATERIAL_TYPE', value);
+  };
+
+  onChangeHandlerText = value => {
+    const { changeFormData, sagaKey } = this.props;
+    changeFormData(sagaKey, 'MATERIAL_TEXT', value);
   };
 
   render() {
@@ -35,18 +62,28 @@ class RadioMaterialComp extends Component {
       <table>
         <tr>
           <td>
-            <Radio.Group name="radiogroup" value={colData} onChange={this.onChangeHanlder}>
+            <Radio.Group name="radiogroup" value={colData} onChange={this.onChangeHandler}>
               <Radio value="Y">Yes</Radio>
               <Radio value="N">No</Radio>
             </Radio.Group>
           </td>
           <td>
-            <Select placeholder="자재코드 선택" style={{ width: '180px' }}>
-              {this.state.mList}
-            </Select>
+            {this.state.isMeterialView && (
+              <Select onChange={this.onSelectChange} placeholder="자재코드 선택" style={{ width: '180px' }}>
+                {this.state.mList}
+              </Select>
+            )}
           </td>
           <td>
-            <Input></Input>
+            {this.state.isMeterialView && (
+              <Input
+                onChange={e => {
+                  const value = e.target.value.replace(/[^0-9,]/gi, '');
+                  e.target.value = value;
+                  this.onChangeHandlerText(value);
+                }}
+              ></Input>
+            )}
           </td>
         </tr>
       </table>
