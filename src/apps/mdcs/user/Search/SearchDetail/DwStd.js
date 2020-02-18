0426dcf0f5b1c5f8524e8b5d99b9423441ef59e0
lesm74 <sangmin.biz@gmail.com>
2020-02-18 15:30:52 +0900

    검색 및 검색 wherestring 수정

--- src/apps/mdcs/user/Search/SearchDetail/DwStd.js
+++ src/apps/mdcs/user/Search/SearchDetail/DwStd.js
@@ -1,168 +1,48 @@
 import React, { Component } from 'react';
 import { Form, Select } from 'antd';
+
 import PropTypes from 'prop-types';
 
+import StyledHtmlTable from 'commonStyled/Table/StyledHtmlTable';
 const FormItem = Form.Item;
 const { Option } = Select;
 
 class DwStd extends Component {
-  componentDidMount() {
-    const { searchParam, setSearchParam } = this.props;
-    const apiArr = [
-      {
-        key: 'dwType',
-        url: '/api/admin/v1/common/categoryMapList?MAP_ID=20',
-        type: 'GET',
-        params: {},
-      },
-      {
-        key: 'product',
-        url: '/api/admin/v1/common/categoryMapList?MAP_ID=16',
-        type: 'GET',
-        params: {},
-      },
-      {
-        key: 'pkg',
-        url: '/api/admin/v1/common/categoryMapList?MAP_ID=15',
-        type: 'GET',
-        params: {},
-      },
-      {
-        key: 'fab',
-        url: '/api/admin/v1/common/categoryMapList?MAP_ID=11',
-        type: 'GET',
-        params: {},
-      },
-      {
-        key: 'lead',
-        url: '/api/admin/v1/common/categoryMapList?MAP_ID=21',
-        type: 'GET',
-        params: {},
-      },
-      {
-        key: 'ball',
-        url: '/api/admin/v1/common/categoryMapList?MAP_ID=22',
-        type: 'GET',
-        params: {},
-      },
-    ];
-    this.callApi(apiArr);
-    setSearchParam({
-      ...searchParam,
-      dwType: { key: 'TBD.DW_TYPE', condition: '=', value: 0, type: 'INT' },
-      product: { key: 'TBD.PRODUCT', condition: '=', value: 0, type: 'INT' },
-      pkg: { key: 'TBD.PKG', condition: '=', value: 0, type: 'INT' },
-      fab: { key: 'TBD.FAB', condition: '=', value: 0, type: 'INT' },
-      lead: { key: 'TBD.LEAD', condition: '=', value: 0, type: 'INT' },
-      ball: { key: 'TBD.BALL', condition: '=', value: 0, type: 'INT' },
-    });
-  }
-
-  callApi = apiArr => {
-    const { sagaKey: id, getCallDataHandler } = this.props;
-    getCallDataHandler(id, apiArr);
-  };
-
   render() {
-    const { result, searchParam, onChangeCheckBox, onChangeValue } = this.props;
-    const { dwType, product, pkg, fab, lead, ball } = searchParam;
-    let dwTypeData = [];
-    let productData = [];
-    let pkgData = [];
-    let fabData = [];
-    let leadData = [];
-    let ballData = [];
-    if (result && result.dwType && result.dwType.categoryMapList && result.dwType.categoryMapList.length > 0) {
-      dwTypeData = result.dwType.categoryMapList.filter(fNode => fNode.LVL > 0 && fNode.USE_YN === 'Y');
-    }
-    if (result && result.product && result.product.categoryMapList && result.product.categoryMapList.length > 0) {
-      productData = result.product.categoryMapList.filter(fNode => fNode.LVL > 0 && fNode.USE_YN === 'Y');
-    }
-    if (result && result.pkg && result.pkg.categoryMapList && result.pkg.categoryMapList.length > 0) {
-      pkgData = result.pkg.categoryMapList.filter(fNode => fNode.LVL > 0 && fNode.USE_YN === 'Y');
-    }
-    if (result && result.fab && result.fab.categoryMapList && result.fab.categoryMapList.length > 0) {
-      fabData = result.fab.categoryMapList.filter(fNode => fNode.LVL > 0 && fNode.USE_YN === 'Y');
-    }
-    if (result && result.lead && result.lead.categoryMapList && result.lead.categoryMapList.length > 0) {
-      leadData = result.lead.categoryMapList.filter(fNode => fNode.LVL > 0 && fNode.USE_YN === 'Y');
-    }
-    if (result && result.ball && result.ball.categoryMapList && result.ball.categoryMapList.length > 0) {
-      ballData = result.ball.categoryMapList.filter(fNode => fNode.LVL > 0 && fNode.USE_YN === 'Y');
-    }
     return (
-      <>
-        <FormItem label="도면구분">
-          {dwTypeData && dwTypeData.length > 0 && (
-            <Select value={(dwType && dwType.value) || 0} onSelect={value => onChangeValue('dwType', value)}>
-              <Option value={0}>--------</Option>
-              {dwTypeData.map(node => (
-                <Option value={node.NODE_ID}>{node.NAME_ENG}</Option>
-              ))}
-            </Select>
-          )}
-        </FormItem>
-        <FormItem label="Product">
-          {productData && productData.length > 0 && (
-            <Select value={(product && product.value) || 0} onSelect={value => onChangeValue('product', value)}>
-              <Option value={0}>--------</Option>
-              {productData.map(node => (
-                <Option value={node.NODE_ID}>{node.NAME_KOR}</Option>
-              ))}
-            </Select>
-          )}
-        </FormItem>
-        <FormItem label="Pkg">
-          {pkgData && pkgData.length > 0 && (
-            <Select value={(pkg && pkg.value) || 0} onSelect={value => onChangeValue('pkg', value)}>
-              <Option value={0}>--------</Option>
-              {pkgData.map(node => (
-                <Option value={node.NODE_ID}>{node.NAME_KOR}</Option>
-              ))}
-            </Select>
-          )}
-        </FormItem>
-        <FormItem label="적용Line/Site">
-          {fabData && fabData.length > 0 && (
-            <Select value={(fab && fab.value) || 0} onSelect={value => onChangeValue('fab', value)}>
-              <Option value={0}>--------</Option>
-              {fabData.map(node => (
-                <Option value={node.NODE_ID}>{node.NAME_KOR}</Option>
-              ))}
-            </Select>
-          )}
-        </FormItem>
-        <FormItem label="적용 Lead">
-          {leadData && leadData.length > 0 && (
-            <Select value={(lead && lead.value) || 0} onSelect={value => onChangeValue('lead', value)}>
-              <Option value={0}>--------</Option>
-              {leadData.map(node => (
-                <Option value={node.NODE_ID}>{node.NAME_KOR}</Option>
-              ))}
-            </Select>
-          )}
-        </FormItem>
-        <FormItem label="적용 Ball">
-          {ballData && ballData.length > 0 && (
-            <Select value={(ball && ball.value) || 0} onSelect={value => onChangeValue('ball', value)}>
-              <Option value={0}>--------</Option>
-              {ballData.map(node => (
-                <Option value={node.NODE_ID}>{node.NAME_KOR}</Option>
-              ))}
-            </Select>
-          )}
-        </FormItem>
-      </>
+      <StyledHtmlTable>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="4">기술표준 정보선택</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>중분류</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>SCOPE</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>Change</th>
+              <td></td>
+            </tr>
+            <tr>
+              <th>FMEA 대상</th>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </StyledHtmlTable>
     );
   }
 }
 
-DwStd.propTypes = {
-  searchParam: PropTypes.objectOf(PropTypes.object, PropTypes.object),
-};
+DwStd.propTypes = {};
 
-DwStd.defaultProps = {
-  searchParam: { nodeIds: { value: [] }, scope: { value: 0 } },
-};
+DwStd.defaultProps = {};
 
 export default DwStd;
