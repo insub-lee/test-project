7df78cb4f266e7dd24e3e2b45abc8ca00efde8e9
hyoseong3291 <wjdgytjd3291@gmail.com>
2020-02-20 22:28:56 +0900

    basicCode app 추가

---
+++ src/apps/eshs/admin/codeAdmin/swCode/basicCode/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BizMicroDevBase from 'components/BizMicroDevBase';
+import List from '../../CmCdList';
+
+class EshsBasicCode extends Component {
+  componantDidMount() {}
+
+  render() {
+    return <BizMicroDevBase sagaKey="eshsBasicCode" component={List} tableName="SW" />;
+  }
+}
+
+EshsBasicCode.propTypes = {};
+EshsBasicCode.defaultProps = {};
+
+export default EshsBasicCode;
