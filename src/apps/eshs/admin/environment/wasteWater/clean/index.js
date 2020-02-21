f3f713f719d70ebf6839c389539140348320aa44
hyoseong3291 <wjdgytjd3291@gmail.com>
2020-02-21 17:21:30 +0900

    eshs 기본코드 관리 정리

---
+++ src/apps/eshs/admin/environment/wasteWater/clean/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BizMicroDevBase from 'components/BizMicroDevBase';
+import List from './List';
+
+class EshsBasicCode extends Component {
+  componantDidMount() {}
+
+  render() {
+    return <BizMicroDevBase sagaKey="eshsBasicCode" component={List} />;
+  }
+}
+
+EshsBasicCode.propTypes = {};
+EshsBasicCode.defaultProps = {};
+
+export default EshsBasicCode;
