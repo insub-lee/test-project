e4a318606d81d57159e605bca7fc1cada7b861ac
hsso <hsso@kb-sys.co.kr>
2020-02-25 13:59:54 +0900

    roadmap input 수정 중

--- src/apps/eshs/user/roadmap/InputPage/index.js
+++ src/apps/eshs/user/roadmap/InputPage/index.js
@@ -18,7 +18,8 @@ class InputPage extends Component {
     super(props);
     this.state = {
       initLoading: true,
-      tempFormData: {},
+      C1: '',
+      H3: '',
     };
   }
 
@@ -38,7 +39,7 @@ class InputPage extends Component {
     }
     changeFormData(id, 'CATEGORY', category);
     if (Moment(month).format('MM') >= 12) {
-      changeFormData(id, 'CHK_DATE', `${Number(Moment(year).format('YYYY')) + 1}/${Moment('01').format('MM')}`);
+      changeFormData(id, 'CHK_DATE', `${Number(Moment(year).format('YYYY')) + 1}/01`);
     } else {
       changeFormData(id, 'CHK_DATE', `${Moment(year).format('YYYY')}/${Number(Moment(month).format('MM')) + 1}`);
     }
@@ -46,28 +47,32 @@ class InputPage extends Component {
 
   saveTask = (id, reloadId) => {
     const { saveTask, saveTaskAfterCallbackFunc, changeFormData } = this.props;
-    this.setState({
-      tempFormData: this.props.formData,
-    });
-    changeFormData(id, 'SITE', 'C1');
     saveTask(id, reloadId, typeof saveTaskAfterCallbackFunc === 'function' ? saveTaskAfterCallbackFunc : this.saveTaskAfter);
   };
-
-  // saveTaskAfter = (id, workSeq, taskSeq, formData) => {
-  //   const { onCloseModleHandler, changeViewPage, baseSagaKey } = this.props;
-  //   if (typeof onCloseModleHandler === 'function') {
-  //     onCloseModleHandler();
-  //     changeViewPage(baseSagaKey, workSeq, -1, 'LIST');
-  //   }
-  // };
 
   saveTaskAfter = (id, workSeq, taskSeq, formData) => {
     const { onCloseModleHandler, changeViewPage, baseSagaKey, changeFormData } = this.props;
     if (typeof onCloseModleHandler === 'function') {
-      console.debug(this.props.getTaskSeq(id, workSeq));
       onCloseModleHandler();
       changeViewPage(baseSagaKey, workSeq, -1, 'LIST');
     }
+  };
+
+  handleOnAddClick = id => {
+    const { changeFormData } = this.props;
+    changeFormData(id, 'VALUE', this.state.C1);
+    changeFormData(id, 'SITE', 'C1');
+    this.saveTask(id, id, this.handleAddCallback(id));
+    console.debug('@@first@@', this.props.formData);
+  };
+
+  handleAddCallback = id => {
+    const { changeFormData } = this.props;
+    changeFormData(id, 'TASK_SEQ', -1);
+    changeFormData(id, 'VALUE', this.state.H3);
+    changeFormData(id, 'SITE', 'H3');
+    this.saveTask(id, id);
+    console.debug('@@second@@', this.props.formData);
   };
 
   render = () => {
@@ -84,7 +89,7 @@ class InputPage extends Component {
       CustomWorkProcess,
     } = this.props;
     // Work Process 사용여부
-    console.debug('@@@@@INPUT@@@@@', this.props);
+    console.debug(this.props.formData.CHK_DATE, this.state.CHK_DATE);
     const isWorkflowUsed = !!(workInfo && workInfo.OPT_INFO && workInfo.OPT_INFO.findIndex(opt => opt.OPT_SEQ === WORKFLOW_OPT_SEQ) !== -1);
     if (viewLayer.length === 1 && viewLayer[0].CONFIG && viewLayer[0].CONFIG.length > 0 && isJSON(viewLayer[0].CONFIG)) {
       const viewLayerData = JSON.parse(viewLayer[0].CONFIG).property || {};
@@ -102,25 +107,6 @@ class InputPage extends Component {
           () => loadingComplete(),
         );
       }
-      // return (
-      //   <StyledViewDesigner>
-      //     <Sketch {...bodyStyle}>
-      //       {isWorkflowUsed &&
-      //         PRC_ID !== -1 &&
-      //         (typeof CustomWorkProcess === 'function' ? (
-      //           <CustomWorkProcess id={id} PRC_ID={PRC_ID} processRule={processRule} setProcessRule={setProcessRule} />
-      //         ) : (
-      //           <WorkProcess id={id} PRC_ID={PRC_ID} processRule={processRule} setProcessRule={setProcessRule} />
-      //         ))}
-      //       <View key={`${id}_${viewPageData.viewType}`} {...this.props} />
-      //       <div className="alignRight">
-      //         <StyledButton className="btn-primary" onClick={() => this.saveTask(id, id)}>
-      //           저장
-      //         </StyledButton>
-      //       </div>
-      //     </Sketch>
-      //   </StyledViewDesigner>
-      // );
       return (
         <StyledViewDesigner>
           <Sketch {...bodyStyle}>
@@ -136,20 +122,20 @@ class InputPage extends Component {
             <hr />
             <Row>
               <Col span={3}>연/월</Col>
-              <Col span={18}>{`${Moment(this.props.year).format('YYYY')}/${Moment(this.props.month).format('MM')}`}</Col>
+              <Col span={18}>{`${Moment(this.props.formData.CHK_DATE).format('YYYY')}/${Moment(this.props.formData.CHK_DATE).format('MM')}`}</Col>
             </Row>
             <hr />
             <Row>
               <Col span={3}>청주</Col>
               <Col span={8}>
-                <InputNumber min={0} />
+                <InputNumber name="C1" min={0} onChange={value => this.setState({ C1: value })} />
               </Col>
               <Col span={3}>구미</Col>
               <Col span={8}>
-                <InputNumber min={0} />
+                <InputNumber name="H3" min={0} onChange={value => this.setState({ H3: value })} />
               </Col>
             </Row>
-            <StyledButton className="btn-primary" onClick={() => this.saveTask(id, id)}>
+            <StyledButton className="btn-primary" onClick={() => this.handleOnAddClick(id)}>
               저장
             </StyledButton>
           </Sketch>
