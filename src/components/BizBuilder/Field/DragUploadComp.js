eacb0c16beb93f56b2c8e449959615acbf6b2299
loganlee94 <dlfjgkssk1@naver.com>
2020-03-10 16:29:14 +0900

    DragUploadComp 소스를 MDCS소스로 덮어씀.

--- src/components/BizBuilder/Field/DragUploadComp.js
+++ src/components/BizBuilder/Field/DragUploadComp.js
@@ -13,378 +13,147 @@ const { Dragger } = Upload;
 const imgExts = ['jpg', 'png', 'gif', 'jpeg'];
 
 class DragUploadComp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      percent: 0,
-      fileList: [],
-    };
-  }
+  state = {
+    fileInfo: [],
+  };
 
   componentDidMount() {
-    const { colData, formData } = this.props;
-    let fileList = [];
-    if (colData && colData.DETAIL) {
-      fileList = colData.DETAIL;
-    }
-
-    if (fileList) {
-      this.setState({
-        fileList: fileList.map(file => ({
-          ...file,
-          uid: file.seq,
-          url: imgExts.includes(file.fileExt.toLowerCase()) ? file.link : file.down,
-          status: 'done',
-        })),
-      });
-    }
+    console.debug('componentDidMount');
+    const { WORK_SEQ, COMP_FIELD, COMP_TAG, colData } = this.props;
+    const initfiles = {
+      WORK_SEQ,
+      TASK_SEQ: -1,
+      CONT_SEQ: -1,
+      FIELD_NM: COMP_FIELD,
+      TYPE: COMP_TAG,
+      DETAIL: colData && colData.DETAIL ? colData.DETAIL : [],
+    };
+    this.setState({ fileInfo: initfiles });
   }
 
-  // componentDidUpdate(prevProps) {
-  //   const { colData: prevColData, COMP_FIELD: compField } = prevProps;
-  //   const { colData, COMP_FIELD } = this.props;
-  //
-  //   if (colData && colData.DETAIL && COMP_FIELD) {
-  //     if (!prevColData || !prevColData.DETAIL) {
-  //       const fileList = colData.DETAIL;
-  //       console.debug('@@@ ', fileList);
-  //       if (fileList) {
-  //         console.debug('@@ fileList', fileList);
-  //         this.setState({
-  //           fileList: fileList.map(file => ({
-  //             ...file,
-  //             uid: file.seq,
-  //             url: imgExts.includes(file.fileExt.toLowerCase()) ? file.link : file.down,
-  //             status: 'done',
-  //           })),
-  //         });
-  //       }
-  //     } else {
-  //       let isValid = true;
-  //       colData.DETAIL.forEach(node => {
-  //         if (prevColData.DETAIL.findIndex(iNode => iNode.seq === node.seq) === -1) isValid = false;
-  //       });
-  //       if (!isValid) {
-  //         const fileList = colData.DETAIL;
-  //
-  //         if (fileList) {
-  //           this.setState({
-  //             fileList: fileList.map(file => ({
-  //               ...file,
-  //               uid: file.seq,
-  //               url: imgExts.includes(file.fileExt.toLowerCase()) ? file.link : file.down,
-  //               status: 'done',
-  //             })),
-  //           });
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  getCurrentValueJson = fileList =>
-    fileList
-      .filter(fileObj => fileObj.status === 'done')
-      .map(fileObj => ({
-        ...fileObj,
-        uid: undefined,
-        thumbUrl: undefined,
-        status: undefined,
-      }));
-
-  getCurrentValue = () => {
-    const { fileList } = this.state;
-    const { name, workSeq, taskSeq, contSeq } = this.props;
-    return JSON.stringify({
-      WORK_SEQ: workSeq,
-      TASK_SEQ: taskSeq,
-      CONT_SEQ: contSeq,
-      FIELD_NM: name,
-      ORD: 0,
-      TYPE: 'rich-text-editor',
-      DETAIL: fileList
-        .filter(fileObj => fileObj.status === 'done')
-        .map(fileObj => ({
-          ...fileObj,
-          uid: undefined,
-          thumbUrl: undefined,
-          status: undefined,
-        })),
-    });
+  onProgress = (step, file) => {
+    const { fileInfo } = this.state;
+    const { DETAIL: fileList } = fileInfo;
+    const tmpDetail = fileList.map(fObj => (fObj.uid === file.uid ? { ...fObj, percent: step } : { ...fObj }));
+    const tmpFileInfo = { ...fileInfo, DETAIL: tmpDetail };
+    this.setState({ fileInfo: tmpFileInfo });
   };
 
-  onRemove = (itemindex, e) => {
-    e.stopPropagation();
-    const { sagaKey: id, changeFormData, colData, COMP_FIELD, CONFIG } = this.props;
-    this.setState(
-      prevState => {
-        const { fileList } = prevState;
-        return {
-          fileList: fileList.filter((item, index) => index !== itemindex),
-        };
-      },
-      () => {
-        // const nextColDataDetail = colData.DETAIL.filter((item, index) => index !== itemindex);
-        // changeFormData(id, COMP_FIELD, nextColDataDetail);
-        // desc - 'done'상태인것만 리듀서에 푸쉬
-        changeFormData(id, COMP_FIELD, { ...colData, DETAIL: this.state.fileList.filter(file => file.status === 'done') });
-      },
-    );
+  changeFormDataHanlder = () => {
+    const { sagaKey, changeFormData, COMP_FIELD, WORK_SEQ } = this.props;
+    const { fileInfo } = this.state;
+    changeFormData(sagaKey, COMP_FIELD, fileInfo);
   };
 
-  handleChange = ({ file, fileList }) => {
-    const { uid, response, thumbUrl } = file;
-    const limitSize = 5;
-    // if (fileList.length <= 5) {
-    if (true) {
-      if (response && response.code === 300) {
-        const uploadedFile = {
-          ...response,
-          name: response.fileName,
-          code: undefined,
-        };
-        const fileIndex = fileList.findIndex(fileObj => fileObj.uid === uid);
-        const nextFileList = fileList.map((fileObj, index) => {
-          if (index === fileIndex) {
-            return {
-              ...uploadedFile,
-              uid,
-              thumbUrl,
-              status: 'done',
-            };
-          }
-          return {
-            ...fileObj,
-          };
-        });
-        this.setState({ fileList: nextFileList }, () => {
-          this.handlerAttachChange(this.state.fileList);
-        });
-      } else {
-        this.setState({ fileList }, () => {
-          if (file.status === 'removed') {
-            this.handlerAttachChange(this.state.fileList);
-          }
-        });
-      }
-
-      this.setState({
-        percent: 0,
-      });
-    } else {
-      message.error(<MessageContent>최대 업로드 갯수를 초과하였습니다.</MessageContent>, 2);
+  onUploadComplete = (response, file) => {
+    const { fileInfo } = this.state;
+    const { DETAIL: fileList } = fileInfo;
+    const { fileExt } = response;
+    let doctype = 'file-unknown';
+    switch (fileExt) {
+      case 'pdf':
+        doctype = 'file-pdf';
+        break;
+      case 'xls':
+        doctype = 'file-excel';
+        break;
+      case 'xlsx':
+        doctype = 'file-excel';
+        break;
+      case 'txt':
+        doctype = 'file-text';
+        break;
+      case 'doc':
+        doctype = 'file-word';
+        break;
+      case 'docx':
+        doctype = 'file-word';
+        break;
+      case 'ppt':
+        doctype = 'file-ppt';
+        break;
+      case 'pptx':
+        doctype = 'file-ppt';
+        break;
+      case 'zip':
+        doctype = 'file-zip';
+        break;
+      default:
+        break;
     }
-  };
-
-  handlerAttachChange = fileList => {
-    const { CONFIG, sagaKey: id, changeFormData, changeValidationData, COMP_FIELD, NAME_KOR, WORK_SEQ, colData, COMP_TAG } = this.props;
-    let retVal = [];
-    if (CONFIG.property.isRequired) {
-      changeValidationData(id, COMP_FIELD, fileList.length > 0, fileList.length > 0 ? '' : `${NAME_KOR}는 필수항목 입니다.`);
-    }
-
-    if (colData && typeof colData === 'object' && colData.DETAIL && colData.DETAIL.length > 0) {
-      colData.DETAIL = fileList;
-      retVal = colData;
-    } else if (colData && typeof colData === 'string' && colData.length > 0 && colData.indexOf('{') === 0 && isJSON(colData)) {
-      JSON.parse(colData).DETAIL = fileList;
-      retVal = colData;
-    } else {
-      // retVal = fileList.filter(file => file.status === 'done');
-      retVal = {
-        WORK_SEQ,
-        TASK_SEQ: -1,
-        CONT_SEQ: -1,
-        FIELD_NM: COMP_FIELD,
-        TYPE: COMP_TAG,
-        DETAIL: fileList.filter(file => file.status === 'done'),
-      };
-      // retVal = {
-      //   WORK_SEQ,
-      //   TASK_SEQ: -1,
-      //   CONT_SEQ: -1,
-      //   FIELD_NM: COMP_FIELD,
-      //   TYPE: COMP_TAG,
-      //   DETAIL: detail,
-      // };
-    }
-    changeFormData(id, COMP_FIELD, retVal);
+    const tmpDetail = fileList.map(fl => (fl.uid === file.uid ? { ...fl, ...response, type: doctype } : fl));
+    const tmpFileInfo = { ...fileInfo, DETAIL: tmpDetail };
+    this.setState({ fileInfo: tmpFileInfo }, () => this.changeFormDataHanlder());
   };
 
   customRequest = ({ action, data, file, filename, headers, onError, onProgress, onSuccess, withCredentials }) => {
-    const { fileList } = this.state;
-    const size = fileList.length;
-    // limiter
-    const limit = 5;
-    // if (size + 1 <= 5) {
-    if (true) {
-      const formData = new FormData();
-      if (data) {
-        Object.keys(data).forEach(key => {
-          formData.append(key, data[key]);
-        });
-      }
-      formData.append(filename, file);
+    const { fileInfo } = this.state;
+    const { DETAIL: fileList } = fileInfo;
 
-      const interval = setInterval(() => this.setState({ percent: this.state.percent + 1 }), 1000);
+    const fileItem = { uid: file.uid, seq: 0, fileName: file.name, fileType: 1, size: file.size, fileExt: '', down: '', percent: 0, type: '' };
+    fileList.push(fileItem);
+    const tmpFileInfo = { ...fileInfo, DETAIL: fileList };
+    this.setState({ fileInfo: tmpFileInfo });
+    const formData = new FormData();
+    formData.append(file.uid, file);
 
-      axios
-        .post(action, formData, {
-          withCredentials,
-          headers,
-          onUploadProgress: ({ total, loaded }) => {
-            onProgress({ percent: Number(Math.round((loaded / total) * 100).toFixed(2)) }, file);
-            this.setState({
-              percent: Number(Math.round((loaded / total) * 100).toFixed(2)),
-            });
-          },
-        })
-        .then(({ data: response }) => {
-          onSuccess(response, file);
-          clearInterval(interval);
-        })
-        .catch(onError);
-    }
-
-    return {
-      abort() {
-        // console.debug('upload progress is aborted.');
-      },
-    };
-  };
-
-  bytesToSize = bytes => {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return '0 Byte';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
+    axios
+      .post(action, formData, {
+        withCredentials,
+        headers,
+        onUploadProgress: ({ total, loaded }) => {
+          const step = Math.round((loaded * 100) / total);
+          onProgress(step, file);
+        },
+      })
+      .then(({ data: response }) => {
+        onSuccess(response, file);
+        this.onUploadComplete(response, file);
+      })
+      .catch(onError);
   };
 
   render() {
-    const { readOnly, visible } = this.props;
-    const { fileList } = this.state;
+    const {
+      fileInfo: { DETAIL: fileList },
+    } = this.state;
 
-    let view = false;
-    if (readOnly !== undefined && readOnly) {
-      view = readOnly;
-    }
-
-    const successFileList = fileList.filter(item => item.status === 'done');
-
-    return visible ? (
-      <StyledDragger>
-        {!view && (
-          <>
-            {/*
-            <div className="btnTypeUploader" style={{ marginBottom: '10px' }}>
-              <Upload
-                action="/upload"
-                fileList={fileList}
-                disabled={!(successFileList.length <= 5)}
-                onChange={this.handleChange}
-                customRequest={this.customRequest}
-                showUploadList={false}
-                multiple
-              >
-                <StyledButton className="btn-light btn-sm">
-                  <Icon type="upload" /> Click to Upload
-                </StyledButton>
-              </Upload>
-            </div>
-            */}
-            <div className="dragTypeUploader" style={{ height: 100 }} onDragEnter={e => e.stopPropagation()} onDragOver={e => e.stopPropagation()}>
-              <Dragger
-                action="/upload"
-                fileList={fileList}
-                // disabled={!(successFileList.length <= 5)}
-                onChange={this.handleChange}
-                customRequest={this.customRequest}
-                showUploadList={false}
-                multiple
-              >
-                <>
-                  {fileList.length === 0 && (
-                    <>
-                      <p className="ant-upload-drag-icon">
-                        <Icon type="inbox" />
-                      </p>
-                      <p className="ant-upload-text">드래그 앤 드롭 또는 클릭</p>
-                    </>
+    return (
+      <div onDragEnter={e => e.stopPropagation()} onDragOver={e => e.stopPropagation()}>
+        <Dragger
+          action="/upload/mdcs"
+          // beforeUpload={this.beforeUpload}
+          onProgress={this.onProgress}
+          customRequest={this.customRequest}
+          onChange={this.onChangeDragger}
+          showUploadList={false}
+          multiple
+        >
+          {fileList && fileList.length > 0 ? (
+            <div className="fileZone">
+              {fileList.map(file => (
+                <div style={{ height: '25px' }}>
+                  {file.percent === 100 ? (
+                    <Icon type={file.type} style={{ fontSize: '20px', marginRight: '5px' }} />
+                  ) : (
+                    <Progress type="circle" width={23} percent={file.percent} style={{ marginRight: '10px' }}></Progress>
                   )}
-                  {fileList.length > 0 && (
-                    <div className="uploadList" style={{ margin: 0, padding: 20, height: 80, overflowY: 'auto' }}>
-                      {fileList.map((file, index) => (
-                        <div className="uploadFileRow" style={{ position: 'relative', height: '25px' }}>
-                          <div className="uploadFileInfo" style={{ position: 'absolute', top: 1, left: 10, fontSize: '0.8rem' }}>
-                            <Icon type="paper-clip" /> {file.name} ({this.bytesToSize(file.size)})
-                          </div>
-                          {file.status === 'done' ? (
-                            <div className="uploadFileBtn" style={{ position: 'absolute', top: 0, right: 10 }}>
-                              <StyledButton className="btn-primary btn-xs btn-first" onClick={() => (window.location.href = `${file.down}`)}>
-                                <Icon type="download" />
-                              </StyledButton>
-                              <StyledButton className="btn-light btn-xs" onClick={e => this.onRemove(index, e)}>
-                                <Icon type="close" />
-                              </StyledButton>
-                            </div>
-                          ) : (
-                            <>
-                              {file.status === 'error' ? (
-                                <div className="uploadFileStatus" style={{ position: 'absolute', top: 4, right: 10 }}>
-                                  <sapn style={{ color: 'red' }}>업로드 실패</sapn>
-                                  {'  '}
-                                  <StyledButton className="btn-light btn-xs" onClick={() => this.onRemove(index)}>
-                                    <Icon type="close" />
-                                  </StyledButton>
-                                </div>
-                              ) : (
-                                <div className="uploadFileStatus" style={{ position: 'absolute', top: '0px', right: '15px', width: '35%' }}>
-                                  <Progress percent={this.state.percent} status="active" />
-                                </div>
-                              )}
-                            </>
-                          )}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </>
-              </Dragger>
-            </div>
-          </>
-        )}
-        {view && (
-          <div className="uploadFileList">
-            {fileList.length > 0 ? (
-              fileList.map((file, index) => (
-                <div className="uploadFileRow" style={{ padding: '10px 10px 10px', position: 'relative', height: '25px' }}>
-                  <div className="uploadFileInfo" style={{ position: 'absolute', top: 1, left: 10, fontSize: '0.8rem' }}>
-                    <Tooltip placement="topLeft" title={`${file.name}(${this.bytesToSize(file.size)})`} trigger="hover">
-                      <Icon type="paper-clip" />
-                      {file.name} ({this.bytesToSize(file.size)})
-                    </Tooltip>
-                  </div>
-                  <div className="uploadFileBtn" style={{ position: 'absolute', top: 0, right: 10 }}>
-                    <StyledButton
-                      className="btn-primary btn-xs btn-first"
-                      onClick={e => {
-                        e.stopPropagation();
-                        window.location.href = `${file.down}`;
-                      }}
-                    >
-                      <Icon type="download" />
-                    </StyledButton>
-                  </div>
+                  <div style={{ verticalAlign: 'middle', height: '28px', display: 'inline-block' }}>{file.fileName}</div>
+                  <Icon type="delete" style={{ fontSize: '16px', verticalAlign: 'baseline', marginLeft: '10px' }} />
+                  <Icon type="download" style={{ fontSize: '16px', verticalAlign: 'baseline', marginLeft: '5px' }} />
                 </div>
-              ))
-            ) : (
-              <span>첨부된 파일이 없습니다.</span>
-            )}
-          </div>
-        )}
-      </StyledDragger>
-    ) : (
-      ''
+              ))}
+            </div>
+          ) : (
+            <div>
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            </div>
+          )}
+        </Dragger>
+      </div>
     );
   }
 }
