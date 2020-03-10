8df3e4c69ece23f3aa3e6173ab989437889ce1dd
LeeInSub <esub17@naver.com>
2020-03-10 09:14:03 +0900

    eshsEiNoAbno 작업중

--- src/apps/eshs/user/eiNoAbno/pages/MaterialTable.js
+++ src/apps/eshs/user/eiNoAbno/pages/MaterialTable.js
@@ -6,25 +6,38 @@ class MaterialTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      FROM_BUILDING_NM: '',
-      TO_BUILDING_NM: '',
+      initMaterial: {},
     };
   }
 
   handleSaveOnClick = action => {
+    const { id, submitHandlerBySaga, eiMaterialData, initEiMaterial, formData } = this.props;
+    let submitData = {};
+
+    const FROM_BUILDING_NM = formData.FROM_BUILDING_NM || '';
+    const TO_BUILDING_NM = formData.TO_BUILDING_NM || '';
+
     if (action === 'SAVE') {
+      submitData = { ...initEiMaterial, FROM_BUILDING_NM, TO_BUILDING_NM };
     } else if (action === 'UPDATE') {
+      submitData = { ...eiMaterialData, FROM_BUILDING_NM, TO_BUILDING_NM };
     }
+    submitHandlerBySaga(id, 'PUT', '/api/eshs/v1/common/eshsEiNoAbno', submitData, this.handleReload);
+  };
+
+  handleReload = () => {
+    console.debug('저장완료.');
   };
 
   handleInputOnChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+    const { id, changeFormData } = this.props;
+    changeFormData(id, `${e.target.name}`, e.target.value);
   };
 
   render() {
-    const { divisionInfo, profile, deptFirstUserInfo, elMaterialData, searchFlag } = this.props;
+    const { initEiMaterial, eiMaterialData, searchFlag, formData } = this.props;
+    const FROM_BUILDING_NM = formData.FROM_BUILDING_NM || '';
+    const TO_BUILDING_NM = formData.TO_BUILDING_NM || '';
     if (searchFlag) {
       return (
         <div className="MaterialTable">
@@ -107,7 +120,7 @@ class MaterialTable extends Component {
         </div>
       );
     }
-    if (JSON.stringify(elMaterialData) == '{}') {
+    if (JSON.stringify(eiMaterialData) == '{}') {
       return (
         <div className="MaterialTable">
           <table>
@@ -124,19 +137,19 @@ class MaterialTable extends Component {
                   <span>사업부명</span>
                 </td>
                 <td>
-                  <Input name="division_nm" value={divisionInfo.NAME_KOR || 'null'} readOnly />
+                  <Input name="DIVISION_NM" value={initEiMaterial.DIVISION_NM || 'null'} readOnly />
                 </td>
                 <td colSpan={2}>
                   <span>공장/FAB/건물명</span>
                 </td>
                 <td colSpan={2}>
-                  <Input name="FROM_BUILDING_NM" onChange={this.handleInputOnChange} />
+                  <Input name="FROM_BUILDING_NM" value={FROM_BUILDING_NM} onChange={this.handleInputOnChange} />
                 </td>
                 <td>
                   <span>조사대상영역</span>
                 </td>
                 <td>
-                  <Input name="TO_BUILDING_NM" onChange={this.handleInputOnChange} />
+                  <Input name="TO_BUILDING_NM" value={TO_BUILDING_NM} onChange={this.handleInputOnChange} />
                 </td>
               </tr>
               <tr>
@@ -144,19 +157,19 @@ class MaterialTable extends Component {
                   <span>부서명</span>
                 </td>
                 <td>
-                  <Input name="from_dept_nm" value={profile.DEPT_NAME_KOR} readOnly />
+                  <Input name="FROM_DEPT_NM" value={initEiMaterial.FROM_DEPT_NM} readOnly />
                 </td>
                 <td>
                   <span>부서장</span>
                 </td>
                 <td>
-                  <Input name="from_dept_manager_no" value={deptFirstUserInfo.NAME_KOR} readOnly />
+                  <Input name="FROM_DEPT_MANAGER_NO" value={initEiMaterial.FROM_DEPT_MANAGER_NO} readOnly />
                 </td>
                 <td>
                   <span>작성자</span>
                 </td>
                 <td>
-                  <Input name="from_emp_nm" value={profile.NAME_KOR} readOnly />
+                  <Input name="FROM_EMP_NO" value={initEiMaterial.FROM_EMP_NO} readOnly />
                 </td>
                 <td>
                   <span>작성일</span>
@@ -170,19 +183,19 @@ class MaterialTable extends Component {
                   <span>주관부서</span>
                 </td>
                 <td>
-                  <Input name="to_dept_nm" value="환경안전팀" readOnly />
+                  <Input name="TO_DEPT_NM" value="환경안전팀" readOnly />
                 </td>
                 <td>
                   <span>부서장</span>
                 </td>
                 <td>
-                  <Input name="to_dept_manager_nm" value="민기식" readOnly />
+                  <Input name="TO_DEPT_MANAGER_NM" value="민기식" readOnly />
                 </td>
                 <td>
                   <span>검토자</span>
                 </td>
                 <td>
-                  <Input name="to_emp_nm" value="함종호" readOnly />
+                  <Input name="TO_EMP_NM" value="함종호" readOnly />
                 </td>
                 <td>
                   <span>검토일</span>
@@ -196,7 +209,7 @@ class MaterialTable extends Component {
         </div>
       );
     }
-    if (JSON.stringify(elMaterialData) != '{}' && !searchFlag) {
+    if (JSON.stringify(eiMaterialData) != '{}' && !searchFlag) {
       return (
         <div className="MaterialTable">
           <table>
@@ -213,19 +226,19 @@ class MaterialTable extends Component {
                   <span>사업부명</span>
                 </td>
                 <td>
-                  <Input name="DIVISION_NM" value={elMaterialData.DIVISION_NM || 'null'} readOnly />
+                  <Input name="DIVISION_NM" value={eiMaterialData.DIVISION_NM || 'null'} readOnly />
                 </td>
                 <td colSpan={2}>
                   <span>공장/FAB/건물명</span>
                 </td>
                 <td colSpan={2}>
-                  <Input name="FROM_BUILDING_NM" value={elMaterialData.FROM_BUILDING_NM || ''} onChange={this.handleInputOnChange} />
+                  <Input name="FROM_BUILDING_NM" value={FROM_BUILDING_NM} onChange={this.handleInputOnChange} />
                 </td>
                 <td>
                   <span>조사대상영역</span>
                 </td>
                 <td>
-                  <Input name="TO_BUILDING_NM" value={elMaterialData.TO_BUILDING_NM || ''} onChange={this.handleInputOnChange} />
+                  <Input name="TO_BUILDING_NM" value={TO_BUILDING_NM} onChange={this.handleInputOnChange} />
                 </td>
               </tr>
               <tr>
@@ -233,25 +246,25 @@ class MaterialTable extends Component {
                   <span>부서명</span>
                 </td>
                 <td>
-                  <Input name="FROM_DEPT_NM" value={elMaterialData.FROM_DEPT_NM || ''} readOnly />
+                  <Input name="FROM_DEPT_NM" value={eiMaterialData.FROM_DEPT_NM || ''} readOnly />
                 </td>
                 <td>
                   <span>부서장</span>
                 </td>
                 <td>
-                  <Input name="FROM_DEPT_MANAGER_NO" value={elMaterialData.FROM_DEPT_MANAGER_NO || ''} readOnly />
+                  <Input name="FROM_DEPT_MANAGER_NO" value={eiMaterialData.FROM_DEPT_MANAGER_NO || ''} readOnly />
                 </td>
                 <td>
                   <span>작성자</span>
                 </td>
                 <td>
-                  <Input name="FROM_EMP_NM" value={elMaterialData.FROM_EMP_NM || ''} readOnly />
+                  <Input name="FROM_EMP_NM" value={eiMaterialData.FROM_EMP_NM || ''} readOnly />
                 </td>
                 <td>
                   <span>작성일</span>
                 </td>
                 <td>
-                  <span> {elMaterialData.FROM_CREATE_DT}</span>
+                  <span> {eiMaterialData.FROM_CREATE_DT}</span>
                 </td>
               </tr>
               <tr>
@@ -277,7 +290,7 @@ class MaterialTable extends Component {
                   <span>검토일</span>
                 </td>
                 <td>
-                  <span> {elMaterialData.TO_CREATE_DT}</span>
+                  <span> {eiMaterialData.TO_CREATE_DT}</span>
                 </td>
               </tr>
             </tbody>
