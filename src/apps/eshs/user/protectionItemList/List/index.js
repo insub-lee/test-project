eaad515d5a6cd462a542a163630e13cb73fb351e
hsso <hsso@kb-sys.co.kr>
2020-03-11 17:01:28 +0900

    roadmapList 천 단위 콤마, 보호구 목록 검색 완성

--- src/apps/eshs/user/protectionItemList/List/index.js
+++ src/apps/eshs/user/protectionItemList/List/index.js
@@ -4,68 +4,154 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import 'ag-grid-community';
+import { Select, Input } from 'antd';
 
 import Sketch from 'components/BizBuilder/Sketch';
 import StyledViewDesigner from 'components/BizBuilder/styled/StyledViewDesigner';
 import StyledButton from 'components/BizBuilder/styled/StyledButton';
 
+import request from 'utils/request';
+import { debounce } from 'lodash';
+
+const { Option } = Select;
 class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedSite: '',
+      input: '',
       columnDefs: [
         {
           headerName: '품목',
-          field: '',
+          field: 'kind',
+          width: 170,
         },
         {
           headerName: '모델',
-          field: '',
+          field: 'model',
+          width: 200,
         },
         {
           headerName: 'Size',
-          field: '',
+          field: 'size1',
+          width: 130,
         },
         {
           headerName: '검정#',
-          field: '',
+          field: 'app_no',
         },
         {
           headerName: 'Vendor',
-          field: '',
+          field: 'vendor_nm',
+          width: 130,
         },
         {
           headerName: 'Maker',
-          field: '',
+          field: 'maker_nm',
         },
         {
           headerName: '단가',
-          field: '',
+          field: 'unitprice',
+          valueFormatter: this.numberFormatter,
         },
         {
           headerName: '유효기간',
-          field: '',
+          field: 'validity_term',
         },
         {
           headerName: '적정재고',
-          field: '',
+          field: 'properstock',
         },
         {
           headerName: '비고',
-          field: '',
+          field: 'comments',
+          width: 300,
         },
       ],
+      rowData: [],
     };
+    this.getNewRowData = debounce(this.getNewRowData, 300);
   }
 
+  gridOptions = {
+    defaultColDef: {
+      width: 100,
+      resizable: true,
+    },
+  };
+
+  initGridData = () => {
+    this.getProtectionItemList().then(res => this.setState({ rowData: res.response.list }));
+  };
+
+  changeGridData = func => {
+    this.setState({
+      rowData: func.response.list,
+    });
+  };
+
+  getProtectionItemList = async () => {
+    const result = await request({
+      url: '/api/eshs/v1/common/geteshsprotectionitems',
+      method: 'GET',
+    });
+    return result;
+  };
+
+  handleSelectChange = async value => {
+    const { input } = this.state;
+    this.setState({
+      selectedSite: value,
+    });
+    this.getNewRowData(value, input);
+  };
+
+  handleInputChange = e => {
+    const { selectedSite } = this.state;
+    this.setState({
+      input: e.target.value,
+    });
+    this.getNewRowData(selectedSite, e.target.value);
+  };
+
+  getNewRowData = async (site, keyword) => {
+    const result = await request({
+      url: `/api/eshs/v1/common/geteshsprotectionitems?site=${site}&keyword=${keyword}`,
+      method: 'GET',
+    });
+    this.changeGridData(result);
+  };
+
+  numberFormatter = params => params.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
   render() {
-    const { columnDefs } = this.state;
+    const { columnDefs, rowData } = this.state;
+    const { initGridData, gridOptions, handleSelectChange, handleInputChange } = this;
     return (
       <StyledViewDesigner>
         <Sketch>
+          <div>
+            <Select defaultValue="청주" onChange={handleSelectChange}>
+              <Option value="청주">청주</Option>
+              <Option value="구미">구미</Option>
+            </Select>
+            <Input
+              onChange={handleInputChange}
+              style={{ width: '300px', marginRight: '10px', marginLeft: '10px', marginBottom: '10px' }}
+              placeholder="품목을 입력하세요."
+            />
+            <StyledButton className="btn-primary">등록</StyledButton>
+          </div>
           <div style={{ width: '100%', height: '100%' }}>
-            <div className="ag-theme-balham">
-              <AgGridReact defaultColDef={columnDefs} columnDefs={columnDefs} />
+            <div className="ag-theme-balham" style={{ height: '560px' }}>
+              <AgGridReact
+                // defaultColDef={columnDefs}
+                columnDefs={columnDefs}
+                rowData={rowData}
+                onGridReady={initGridData}
+                gridOptions={gridOptions}
+                suppressRowTransform
+              />
             </div>
           </div>
         </Sketch>
--- src/apps/eshs/user/protectionItemList/index.js
+++ src/apps/eshs/user/protectionItemList/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import BizBuilderBase from 'components/BizBuilderBase';
 import List from './List';
 
-const ProtectionItemList = () => <BizBuilderBase sagaKey="ProtectionItemList" workSeq={4561} viewType="LIST" CustomListPage={List} />;
+const ProtectionItemList = () => <BizBuilderBase sagaKey="protectionItemList" workSeq={4561} viewType="LIST" CustomListPage={List} />;
 
 export default ProtectionItemList;
