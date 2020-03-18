e1c6ded5dd19c88c8df42b087c749fb87220b7f5
LeeInSub <esub17@naver.com>
2020-03-18 12:11:31 +0900

    eshs eiAir, eiDeptSearchBar, eiImportantAssesment, eiMaterial, eiMateriamTable, eiNatural, eiStatement, eiWaste, eiStatement, eiWaste, eiWater eia 폴더로 이동..

--- src/apps/eshs/user/eiStatement/MainPage/ItemTable/PopoverContent/index.js
+++
@@ -1,7 +0,0 @@
-import Eee1 from './eee_1.gif';
-import Eee2 from './eee_2.gif';
-import Eee3 from './eee_3.gif';
-import Eee4 from './eee_4.gif';
-import Eee5 from './eee_5.gif';
-
-export { Eee1, Eee2, Eee3, Eee4, Eee5 };
