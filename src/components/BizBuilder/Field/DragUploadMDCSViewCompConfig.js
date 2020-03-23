a08794f284c3cb78fb88b7c73eab51aff7a2ace9
lesm74 <sangmin.biz@gmail.com>
2020-03-23 20:42:48 +0900

    조회화면 개발

--- src/components/BizBuilder/Field/DragUploadMDCSViewCompConfig.js
+++ src/components/BizBuilder/Field/DragUploadMDCSViewCompConfig.js
@@ -1,73 +1,153 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Radio, Select, Input } from 'antd';
-import StyledHtmlTable from 'commonStyled/Table/StyledHtmlTable';
+import StyledHtmlTable from 'commonStyled/MdcsStyled/Table/StyledHtmlTable';
+
+const { Option } = Select;
 
 class DragUploadMDCSViewCompConfig extends Component {
   constructor(props) {
     super(props);
     this.state = {
       downType: undefined,
-      drmInfo: {},
+      drmInfo: { pr: undefined, uc: undefined, bk: undefined, ed: undefined },
+      aclList: [],
+      optList: [],
     };
+  }
+
+  initDataBind = response => {
+    const {
+      configInfo: {
+        property: { selectedValue },
+      },
+    } = this.props;
+    const { aclList: list } = response;
+    const optList = list.map(item => <Option key={item.IDX}>{item.ACL_DESC}</Option>);
+    this.setState({ ...selectedValue, aclList: list, optList });
+  };
+
+  componentDidMount() {
+    const { submitHandlerBySaga, configInfo } = this.props;
+    const apiUrl = '/api/mdcs/v1/common/drmAclHandler';
+    submitHandlerBySaga('GET', apiUrl, {}, this.initDataBind);
   }
 
   onChangeDownType = e => {
     const { value } = e.target;
-    this.setState({ downType: value });
+    this.setState(
+      prevState => ({ ...prevState, downType: value }),
+      () => this.onSaveConfig(),
+    );
+  };
+
+  onChangeDRM = value => {
+    const { aclList } = this.state;
+    const idx = aclList.findIndex(f => f.IDX === Number(value));
+    if (idx > -1) {
+      const selectedAcl = aclList[idx];
+      this.setState({ drmInfo: { pr: selectedAcl.PR, uc: selectedAcl.UC, bk: selectedAcl.BK, ed: selectedAcl.ED } }, () => this.onSaveConfig());
+    }
+  };
+
+  onSaveConfig = () => {
+    const { changeViewCompData, groupIndex, rowIndex, colIndex, configInfo } = this.props;
+    const { downType, drmInfo } = this.state;
+    configInfo.property.selectedValue = { downType, drmInfo };
+    changeViewCompData(groupIndex, rowIndex, colIndex, 'CONFIG', configInfo);
+  };
+
+  onChangePrint = e => {
+    const { value } = e.target;
+    const { drmInfo } = this.state;
+    const tDrmInfo = { ...drmInfo, pr: value };
+    this.setState({ drmInfo: tDrmInfo }, () => this.onSaveConfig());
+  };
+
+  onChangeContorl = e => {
+    const { value } = e.target;
+    const { drmInfo } = this.state;
+    const tDrmInfo = { ...drmInfo, uc: value };
+    this.setState({ drmInfo: tDrmInfo }, () => this.onSaveConfig());
+  };
+
+  onChangeCopyPaste = e => {
+    const { value } = e.target;
+    const { drmInfo } = this.state;
+    const tDrmInfo = { ...drmInfo, bk: value };
+    this.setState({ drmInfo: tDrmInfo }, () => this.onSaveConfig());
+  };
+
+  onChangeExpire = e => {
+    const { value } = e.target;
+    const { drmInfo } = this.state;
+    const tDrmInfo = { ...drmInfo, ed: value };
+    this.setState({ drmInfo: tDrmInfo }, () => this.onSaveConfig());
   };
 
   render() {
-    const { downType } = this.state;
+    const {
+      downType,
+      drmInfo: { pr, uc, bk, ed },
+      optList,
+    } = this.state;
+    console.debug('this.state', this.state);
     return (
       <StyledHtmlTable>
         <table>
-          <tr>
-            <td>다운로드 형식</td>
-            <td>
-              <Radio.Group value={downType} onChange={this.onChangeDownType}>
-                <Radio value="DRM">다운로드(DRM적용)</Radio>
-                <Radio value="PDF">PDF 다운로드</Radio>
-              </Radio.Group>
-            </td>
-          </tr>
-          <tr>
-            <td colSpan={2}>*DRM 설정</td>
-          </tr>
-          <tr>
-            <td>Print</td>
-            <td>
-              <Radio.Group>
-                <Radio>YES</Radio>
-                <Radio>NO</Radio>
-              </Radio.Group>
-            </td>
-          </tr>
-          <tr>
-            <td>Control/Uncontrol</td>
-            <td>
-              <Radio.Group>
-                <Radio>Controlled</Radio>
-                <Radio>UnControlled</Radio>
-                <Radio>Preliminary</Radio>
-              </Radio.Group>
-            </td>
-          </tr>
-          <tr>
-            <td>Copy & Paste</td>
-            <td>
-              <Radio.Group>
-                <Radio>YES</Radio>
-                <Radio>NO</Radio>
-              </Radio.Group>
-            </td>
-          </tr>
-          <tr>
-            <td>Expire Date</td>
-            <td>
-              <Input></Input>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>다운로드 형식</th>
+              <td>
+                <Radio.Group value={downType} onChange={this.onChangeDownType}>
+                  <Radio value="DRM">다운로드(DRM적용)</Radio>
+                  <Radio value="PDF">PDF 다운로드</Radio>
+                </Radio.Group>
+              </td>
+            </tr>
+            <tr>
+              <th>DRM 정책</th>
+              <td>
+                <Select style={{ width: '300px' }} placeholder="DRM 정책을 선택해주세요" onChange={this.onChangeDRM}>
+                  {optList}
+                </Select>
+              </td>
+            </tr>
+            <tr>
+              <th>Print</th>
+              <td>
+                <Radio.Group value={pr} onChange={this.onChangePrint}>
+                  <Radio value="1">YES</Radio>
+                  <Radio value="0">NO</Radio>
+                </Radio.Group>
+              </td>
+            </tr>
+            <tr>
+              <th>Control/Uncontrol</th>
+              <td>
+                <Radio.Group value={uc} onChange={this.onChangeContorl}>
+                  <Radio value="C">Controlled</Radio>
+                  <Radio value="U">UnControlled</Radio>
+                  <Radio value="P">Preliminary</Radio>
+                </Radio.Group>
+              </td>
+            </tr>
+            <tr>
+              <th>Copy & Paste</th>
+              <td>
+                <Radio.Group value={bk} onChange={this.onChangeCopyPaste}>
+                  <Radio value="1">YES</Radio>
+                  <Radio value="0">NO</Radio>
+                </Radio.Group>
+              </td>
+            </tr>
+            <tr>
+              <th>Expire Date</th>
+              <td>
+                <Input value={ed} onChange={this.onChangeExpire}></Input>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </StyledHtmlTable>
     );
