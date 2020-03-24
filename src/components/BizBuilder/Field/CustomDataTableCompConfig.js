f31de2ba0699cdf0d10f3e10373c0719a823fb18
loganlee94 <dlfjgkssk1@naver.com>
2020-03-24 15:34:54 +0900

    CustomDataTableCompConfig 수정

--- src/components/BizBuilder/Field/CustomDataTableCompConfig.js
+++ src/components/BizBuilder/Field/CustomDataTableCompConfig.js
@@ -5,29 +5,31 @@ import { Col, Input, Row, Button, Radio } from 'antd';
 import _, { debounce } from 'lodash';
 
 const cont = {
-  ADD: 'ADD',
-  RESET: 'RESET',
-  REMOVE: 'REMOVE',
-  NAME: 'NAME',
-  OPTION: 'OPTION',
-  RADIO: 'RADIO',
-  TEXT: 'TEXT',
-  GROUP: 'GROUP',
+  ADD_: 'ADD',
+  RESET_: 'RESET',
+  REMOVE_: 'REMOVE',
+  TITLE_: 'TITLE',
+  OPTION_: 'OPTION',
+  RADIO_: 'RADIO',
+  TEXT_: 'TEXT',
+  DATA_INDEX_: 'DATA_INDEX',
+  GROUP_TITLE_: 'GROUP_TITLE',
+  ORDER_: 'ORDER',
 };
 
 function CustomDataTableCompConfig(props) {
-  const { ADD, RESET, REMOVE, NAME, OPTION, RADIO, TEXT, GROUP } = cont;
+  const { ADD_, RESET_, REMOVE_, TITLE_, OPTION_, RADIO_, TEXT_, DATA_INDEX_, GROUP_TITLE_, ORDER_ } = cont;
   const [url, setUrl] = useState(undefined);
-  const [headers, setHeaders] = useState([]);
-  // const [group, setGroup] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [groups, setGroups] = useState([]);
   const [title, setTitle] = useState(undefined);
 
   // componentDidMount
   useEffect(() => {
-    const { URL, HEADERS, TITLE } = props.configInfo.property || {};
+    const { URL, COLUMNS, GROUPS, TITLE } = props.configInfo.property || {};
     setUrl(URL);
-    setHeaders(HEADERS || []);
-    // setGroup(GROUP);
+    setColumns(COLUMNS || []);
+    setGroups(GROUPS || []);
     setTitle(TITLE);
   }, []);
 
@@ -36,15 +38,15 @@ function CustomDataTableCompConfig(props) {
   }, [url]);
 
   useEffect(() => {
-    debouncedHandleChangeViewCompData('HEADERS', headers);
-  }, [headers]);
-
-  // useEffect(() => {
-  //   debouncedHandleChangeViewCompData('GROUP', headers);
-  // }, [group]);
+    debouncedHandleChangeViewCompData('COLUMNS', columns);
+  }, [columns]);
 
   useEffect(() => {
-    debouncedHandleChangeViewCompData('TITLE', headers);
+    debouncedHandleChangeViewCompData('GROUPS', groups);
+  }, [groups]);
+
+  useEffect(() => {
+    debouncedHandleChangeViewCompData('TITLE', title);
   }, [title]);
 
   const { changeViewCompData, groupIndex, rowIndex, colIndex, configInfo } = props;
@@ -56,46 +58,89 @@ function CustomDataTableCompConfig(props) {
 
   const debouncedHandleChangeViewCompData = debounce(handleChangeViewCompData, 250);
 
-  const headerHandler = type => {
-    let temp = [...(headers || [])];
+  const columnSizeHandler = type => {
+    let temp = [...(columns || [])];
     switch (type) {
-      // add header
-      case ADD: {
-        const tempHeader = { name: undefined, group: undefined, option: TEXT };
-        temp.push(tempHeader);
+      // add Column
+      case ADD_: {
+        const tempColumns = { name: undefined, group: undefined, option: TEXT_ };
+        temp.push(tempColumns);
         break;
       }
 
-      // remove header
-      case REMOVE: {
+      // remove Column
+      case REMOVE_: {
         temp = [..._.dropRight(temp, 1)];
         break;
       }
     }
-    setHeaders(temp);
+    setColumns(temp);
   };
 
-  // header name
-  const valueHandler = (type, value, idx) => {
+  const groupSizeHandler = type => {
+    let temp = [...(groups || [])];
+    switch (type) {
+      // add Group
+      case ADD_: {
+        const tempColumns = { title: undefined, group: undefined, option: TEXT_ };
+        temp.push(tempColumns);
+        break;
+      }
+
+      // remove Group
+      case REMOVE_: {
+        temp = [..._.dropRight(temp, 1)];
+        break;
+      }
+    }
+    setGroups(temp);
+  };
+
+  // Column title
+  const columnValueHandler = (type, value, idx) => {
     if (typeof value === 'string') {
+      const temp = [...(columns || [])];
       value = value.trim();
-      const temp = [...(headers || [])];
 
       switch (type) {
-        case NAME: {
-          temp[idx].name = value;
+        case TITLE_: {
+          temp[idx].title = value;
           break;
         }
-        case GROUP: {
-          temp[idx].GROUP = value;
+        case DATA_INDEX_: {
+          temp[idx].dataIndex = value;
           break;
         }
-        case OPTION: {
+        case ORDER_: {
+          temp[idx].order = value;
+          break;
+        }
+        case OPTION_: {
           temp[idx].option = value;
           break;
         }
       }
-      setHeaders(temp);
+      setColumns(temp);
+    }
+  };
+
+  // Group title
+  const groupValueHandler = (type, value, idx) => {
+    if (typeof value === 'string') {
+      value = value.trim();
+      const temp = [...(groups || [])];
+
+      switch (type) {
+        case GROUP_TITLE_: {
+          temp[idx].title = value;
+          break;
+        }
+        case ORDER_: {
+          temp[idx].order = value;
+          break;
+        }
+      }
+      setGroups(temp);
     }
   };
 
@@ -105,65 +150,130 @@ function CustomDataTableCompConfig(props) {
       <Input value={url} onChange={e => setUrl(e.target.value)} placeHolder="대상 URL을 입력" />
     </div>,
     <div className="popoverItem popoverItemInput">
-      <span className="spanLabel">헤더 사이즈</span>
+      <span className="spanLabel">Group 사이즈</span>
       <span style={{ display: 'block', textAlign: 'center' }}>
         <Button
           type="primary"
           style={{ width: '33.3%' }}
           onClick={() => {
-            headerHandler(ADD);
+            groupSizeHandler(ADD_);
           }}
         >
-          {ADD}
+          {ADD_}
         </Button>
         <Button
           type="ghost"
           style={{ width: '33.3%' }}
           onClick={() => {
-            setHeaders([]);
+            setGroups([]);
           }}
         >
-          {RESET}
+          {RESET_}
         </Button>
         <Button
           type="danger"
           style={{ width: '33.3%' }}
           onClick={() => {
-            headerHandler(REMOVE);
+            groupSizeHandler(REMOVE_);
           }}
         >
-          {REMOVE}
+          {REMOVE_}
         </Button>
       </span>
     </div>,
     <>
-      {headers.length > 0 &&
-        headers.map(({ name, option, group }, idx) => (
+      {groups.length > 0 &&
+        groups.map(({ title, order }, idx) => (
           <div className="popoverItem popoverItemInput">
-            <span className="spanLabel">header{idx + 1}</span>
+            <span className="spanLabel">group{idx + 1}</span>
             <span>
               <Input
                 style={{ width: '38%' }}
-                value={group}
-                onChange={e => valueHandler(GROUP, e.target.value, idx)}
-                onKeyDown={e => (e.keyCode === 13 ? headerHandler(ADD) : null)}
+                value={title}
+                placeHolder="Column Group Title"
+                onChange={e => groupValueHandler(GROUP_TITLE_, e.target.value, idx)}
+                onKeyDown={e => (e.keyCode === 13 ? groupSizeHandler(ADD_) : null)}
               />
               <Input
                 style={{ width: '38%' }}
-                value={name}
-                onChange={e => valueHandler(NAME, e.target.value, idx)}
-                onKeyDown={e => (e.keyCode === 13 ? headerHandler(ADD) : null)}
+                value={order}
+                placeHolder="Column Group order"
+                onChange={e => groupValueHandler(ORDER_, e.target.value, idx)}
+                onKeyDown={e => (e.keyCode === 13 ? groupSizeHandler(ADD_) : null)}
+              />
+            </span>
+          </div>
+        ))}
+    </>,
+    <div className="popoverItem popoverItemInput">
+      <span className="spanLabel">Column 사이즈</span>
+      <span style={{ display: 'block', textAlign: 'center' }}>
+        <Button
+          type="primary"
+          style={{ width: '33.3%' }}
+          onClick={() => {
+            columnSizeHandler(ADD_);
+          }}
+        >
+          {ADD_}
+        </Button>
+        <Button
+          type="ghost"
+          style={{ width: '33.3%' }}
+          onClick={() => {
+            setColumns([]);
+          }}
+        >
+          {RESET_}
+        </Button>
+        <Button
+          type="danger"
+          style={{ width: '33.3%' }}
+          onClick={() => {
+            columnSizeHandler(REMOVE_);
+          }}
+        >
+          {REMOVE_}
+        </Button>
+      </span>
+    </div>,
+    <>
+      {columns.length > 0 &&
+        columns.map(({ title, option, order, dataIndex }, idx) => (
+          <div className="popoverItem popoverItemInput">
+            <span className="spanLabel">Column{idx + 1}</span>
+            <span>
+              <Input
+                style={{ width: '25%' }}
+                value={title}
+                placeHolder="Column Title"
+                onChange={e => columnValueHandler(TITLE_, e.target.value, idx)}
+                onKeyDown={e => (e.keyCode === 13 ? columnSizeHandler(ADD_) : null)}
+              />
+              <Input
+                style={{ width: '25%' }}
+                value={dataIndex}
+                placeHolder="Data Index"
+                onChange={e => columnValueHandler(DATA_INDEX_, e.target.value, idx)}
+                onKeyDown={e => (e.keyCode === 13 ? columnSizeHandler(ADD_) : null)}
+              />
+              <Input
+                style={{ width: '25%' }}
+                value={order}
+                placeHolder="Column Group Order"
+                onChange={e => columnValueHandler(ORDER_, e.target.value, idx)}
+                onKeyDown={e => (e.keyCode === 13 ? columnSizeHandler(ADD_) : null)}
               />
               <Radio.Group
-                style={{ width: '24%', textAlign: 'center' }}
+                style={{ width: '20%', textAlign: 'center' }}
                 value={option}
                 onChange={e => {
                   const { value } = e.target;
-                  valueHandler(OPTION, value, idx);
+                  columnValueHandler(OPTION_, value, idx);
                 }}
               >
-                <Radio value={RADIO}>{RADIO}</Radio>
-                <Radio value={TEXT}>{TEXT}</Radio>
+                <Radio value={RADIO_}>{RADIO_}</Radio>
+                <Radio value={TEXT_}>{TEXT_}</Radio>
               </Radio.Group>
             </span>
           </div>
@@ -181,7 +291,7 @@ CustomDataTableCompConfig.propTypes = {
   CONFIG: {
     info: {},
     option: {},
-    property: { URL: undefined, headers: [] },
+    property: { URL: undefined, columns: [] },
   },
   COMP_FIELD: '',
 };
