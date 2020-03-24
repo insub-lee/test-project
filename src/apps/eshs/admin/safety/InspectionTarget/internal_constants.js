8dbbb39499e364b9b92961732bb3aaf8b47c9cd8
loganlee94 <dlfjgkssk1@naver.com>
2020-03-24 11:19:16 +0900

    내부 상수 수정

--- src/apps/eshs/admin/safety/InspectionTarget/internal_constants.js
+++ src/apps/eshs/admin/safety/InspectionTarget/internal_constants.js
@@ -5,7 +5,8 @@ export const address = {
   afterProcessing: `${baseUrl}afterProcessing`,
 };
 export const VIEW_TYPE = { INPUT: 'INPUT', VIEW: 'VIEW', MODIFY: 'MODIFY', LIST: 'LIST' };
-export const META_SEQ = { MODAL_LIST: 3901, VIEW_BASIC: 3681, INSPECTION: 4841, ISSUE: 4842 };
+export const META_SEQ = { MODAL_LIST: 3901, VIEW_BASIC: 3681, VIEW_INSPECTION: 4841, VIEW_ISSUE: 4842, MODIFY_ISSUE: 5121, MODIFY_INSPECTION: 5122 };
+export const VIEW_CHANGE_SEQ = {};
 export const BUTTON_CATEGORY = { FIRE_EXTINGUISHER_MAIN: 'FIRE_EXTINGUISHER' };
 
 export default 'specify what you want to import.';
