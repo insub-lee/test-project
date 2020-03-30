18a343d1b7e137cc2e310fce2118bbe299f4b40a
hyoseong3291 <wjdgytjd3291@gmail.com>
2020-03-30 19:20:15 +0900

    commonStyled/Buttons/StyledButtonWrapper 및 commonStyled/Form/StyledInput

--- src/commonStyled/Buttons/StyledButtonWrapper.js
+++ src/commonStyled/Buttons/StyledButtonWrapper.js
@@ -12,9 +12,19 @@ const btnWrapMt20 = css`
   margin-top: 20px;
 `;
 
+const btnWrapInline = css`
+  display: inline-block;
+  width: auto;
+  padding: 0;
+  margin-left: 10px;
+`;
+
 const StyledButtonWrapper = styled.div`
   width: 100%;
   padding: 10px 0;
+  &.btn-wrap-inline {
+    ${btnWrapInline}
+  }
   &.btn-wrap-right {
     ${btnWrapRight}
   }
