6c5f0a3fc9bab13641e260202a12d33e28fa87d2
LoganLee <chlee@kb-sys.co.kr>
2020-04-02 15:09:45 +0900

    add search API address

--- src/apps/eshs/admin/safety/InspectionTarget/internal_constants.js
+++ src/apps/eshs/admin/safety/InspectionTarget/internal_constants.js
@@ -3,6 +3,7 @@ export const address = {
   selectAllData: `${baseUrl}fireInspection`,
   generatePositionNo: `${baseUrl}generatePositionNo`,
   afterProcessing: `${baseUrl}afterProcessing`,
+  search: `${baseUrl}search`,
 };
 export const VIEW_TYPE = { INPUT: 'INPUT', VIEW: 'VIEW', MODIFY: 'MODIFY', LIST: 'LIST' };
 export const META_SEQ = { MODAL_LIST: 3901, VIEW_BASIC: 3681, VIEW_INSPECTION: 4841, VIEW_ISSUE: 4842, MODIFY_ISSUE: 5121, MODIFY_INSPECTION: 5122 };
