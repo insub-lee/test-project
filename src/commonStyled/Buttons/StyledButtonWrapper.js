fb6a35bc4e0b446e0b277e01c9e691161023ad8f
icyu <icyu@kb-sys.co.kr>
2020-04-03 14:56:09 +0900

    ButtonWrapper 로직 수정(변수형태 제거)

--- src/commonStyled/Buttons/StyledButtonWrapper.js
+++ src/commonStyled/Buttons/StyledButtonWrapper.js
@@ -1,38 +1,24 @@
 import styled, { css } from 'styled-components';
 
-const btnWrapRight = css`
-  text-align: right;
-`;
-
-const btnWrapCenter = css`
-  text-align: center;
-`;
-
-const btnWrapMt20 = css`
-  margin-top: 20px;
-`;
-
-const btnWrapInline = css`
-  display: inline-block;
-  width: auto;
-  padding: 0;
-  margin-left: 10px;
-`;
-
 const StyledButtonWrapper = styled.div`
   width: 100%;
-  padding: 10px 0;
+
   &.btn-wrap-inline {
-    ${btnWrapInline}
+    display: inline-block;
+    width: auto;
+    padding: 0;
   }
+
   &.btn-wrap-right {
-    ${btnWrapRight}
+    text-align: right;
   }
+
   &.btn-wrap-center {
-    ${btnWrapCenter}
+    text-align: center;
   }
+
   &.btn-wrap-mt-20 {
-    ${btnWrapMt20}
+    margin-top: 20px;
   }
 `;
 
