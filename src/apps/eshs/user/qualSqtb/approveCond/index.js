abf2dbcba16ee4c0b24242853809a5122c9ac8e1
LeeInSub <esub17@naver.com>
2020-04-16 20:51:05 +0900

    antdTable Column render 부분 Input, select  defaultValue 사용할경우 pagenation 이슈 임시로 reload시키는 방식으로 수정 css 추가후  y축으로 늘어나도록 수정할예정

--- src/apps/eshs/user/qualSqtb/approveCond/index.js
+++ src/apps/eshs/user/qualSqtb/approveCond/index.js
@@ -17,6 +17,7 @@ class ApproveCond extends Component {
     super(props);
     this.state = {
       approveCondTable: [],
+      defaultCurrent: 1,
       initApprove: {
         REQ_CD: this.props && this.props.formData && this.props.formData.REQ_CD,
         TASK_SEQ: this.props && this.props.formData && this.props.formData.TASK_SEQ,
@@ -166,7 +167,7 @@ class ApproveCond extends Component {
 
   debounceHandelSetTable = () => {
     const { formData, id } = this.props;
-    const { columns } = this.state;
+    const { columns, defaultCurrent } = this.state;
     const approveCondList = (formData && formData.approveCondList) || [];
     return this.setState({
       approveCondTable: [
@@ -177,7 +178,12 @@ class ApproveCond extends Component {
           columns={columns}
           dataSource={approveCondList || []}
           bordered
-          pagination={{ pageSize: 6 }}
+          pagination={{
+            hideOnSinglePage: false,
+            defaultCurrent,
+            pageSize: 6,
+            onChange: page => this.setState({ defaultCurrent: page, approveCondTable: [] }, this.debounceHandelSetTable),
+          }}
           footer={() => <span>{`${approveCondList.length} 건`}</span>}
         />,
       ],
