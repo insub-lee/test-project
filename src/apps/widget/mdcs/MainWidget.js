d5a9c5e1b2bf20aa7bba25307a499b7f1f76f765
lesm74 <sangmin.biz@gmail.com>
2020-04-20 19:08:02 +0900

    mdcs 메인화면

---
+++ src/apps/widget/mdcs/MainWidget.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainWidget extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return 'TEST!!!!!';
+  }
+}
+
+export default MainWidget;
