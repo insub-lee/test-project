a2815b52e146798c7fb371138c7ebcc0178655df
hsso <hsso@kb-sys.co.kr>
2020-04-23 22:34:53 +0900

    특수건강진단대상 유해인자 추가, 버그 수정

--- src/apps/eshs/user/environmentChemicalMaterialSafetyActSpecialHealth/List/index.js
+++ src/apps/eshs/user/environmentChemicalMaterialSafetyActSpecialHealth/List/index.js
@@ -26,7 +26,6 @@ class List extends React.Component {
     super(props);
     this.state = {
       categories: [],
-      subCategories: [],
       visible: false,
       subTableVisible: false,
       requestValue: {
@@ -34,10 +33,9 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: '',
-        SUB_CATEGORY: '',
         IS_APPLICATE: 'Y',
         SERIAL_NO: '',
-        WORK_ID: '',
+        HEALTH_ID: '',
         CONTENT_STANDARD: '0',
       },
       subRequestValue: {
@@ -45,9 +43,8 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: '',
-        SUB_CATEGORY: '',
         IS_APPLICATE: 'Y',
-        WORK_ID: '',
+        HEALTH_ID: '',
         CONTENT_STANDARD: '0',
         PARENT_ID: '',
       },
@@ -70,7 +67,7 @@ class List extends React.Component {
         key: 'codeCategory',
         type: 'POST',
         url: `/api/admin/v1/common/categoryMapList`,
-        params: { PARAM: { NODE_ID: 1951 } },
+        params: { PARAM: { NODE_ID: 1953 } },
       },
     ];
 
@@ -79,7 +76,7 @@ class List extends React.Component {
 
   setCategory = () => {
     const { result } = this.props;
-    const category = result.codeCategory.categoryMapList.filter(item => item.PARENT_NODE_ID === 1951);
+    const category = result.codeCategory.categoryMapList.slice(1);
     this.setState({
       categories: category,
     });
@@ -94,14 +91,6 @@ class List extends React.Component {
   handleInputChange = (event, type, name) => {
     if (type.toUpperCase() === 'INPUT') {
       const valueObj = { [event.target.name.toUpperCase()]: event.target.value };
-      return this.setState(prevState => ({
-        requestValue: Object.assign(prevState.requestValue, valueObj),
-      }));
-    }
-
-    if (type.toUpperCase() === 'SELECT' && name === 'CATEGORY') {
-      const valueObj = { [name.toUpperCase()]: event, SUB_CATEGORY: '' };
-      this.getSubCategories(event);
       return this.setState(prevState => ({
         requestValue: Object.assign(prevState.requestValue, valueObj),
       }));
@@ -131,14 +120,6 @@ class List extends React.Component {
       }));
     }
 
-    if (type.toUpperCase() === 'SELECT' && name === 'CATEGORY') {
-      const valueObj = { [name.toUpperCase()]: event, SUB_CATEGORY: '' };
-      this.getSubCategories(event);
-      return this.setState(prevState => ({
-        subRequestValue: Object.assign(prevState.subRequestValue, valueObj),
-      }));
-    }
-
     if (type.toUpperCase() === 'SELECT') {
       const valueObj = { [name.toUpperCase()]: event };
       return this.setState(prevState => ({
@@ -155,22 +136,6 @@ class List extends React.Component {
     return null;
   };
 
-  getSubCategories = value => {
-    const { result } = this.props;
-    const category = result.codeCategory.categoryMapList.filter(item => item.PARENT_NODE_ID === value);
-    console.debug('@@@@@@GET SUB CATEGORIES@@@@@@', value, typeof value, category);
-    this.setState({
-      subCategories: category,
-    });
-  };
-
-  setSubCategories = () => {
-    const { result } = this.props;
-    this.setState({
-      subCategories: (result.subCategories && result.subCategories.categoryMapList && result.subCategories.categoryMapList.slice(1)) || [],
-    });
-  };
-
   handleInputClick = () => {
     const { sagaKey: id, submitHandlerBySaga } = this.props;
     const { requestValue, isModified, dataSource } = this.state;
@@ -181,7 +146,7 @@ class List extends React.Component {
       return submitHandlerBySaga(
         id,
         'PUT',
-        `/api/eshs/v1/common/eshschemicalsafetyworkplace`,
+        `/api/eshs/v1/common/eshschemicalsafetyspecialhealth`,
         { requestValue, SUB_MATERIALS: dataSource.slice(1) },
         this.getMaterialList,
       );
@@ -192,7 +157,7 @@ class List extends React.Component {
     return submitHandlerBySaga(
       id,
       'POST',
-      `/api/eshs/v1/common/eshschemicalsafetyworkplace`,
+      `/api/eshs/v1/common/eshschemicalsafetyspecialhealth`,
       { requestValue, SUB_MATERIALS: dataSource.slice(1) },
       this.getMaterialList,
     );
@@ -200,7 +165,7 @@ class List extends React.Component {
 
   handleDeleteClick = () => {
     const { requestValue } = this.state;
-    if (!requestValue.WORK_ID) {
+    if (!requestValue.HEALTH_ID) {
       return this.setState({
         deleteConfirmMessage: '선택된 항목이 없습니다.',
       });
@@ -213,7 +178,7 @@ class List extends React.Component {
   handleDeleteConfirm = () => {
     const { sagaKey: id, submitHandlerBySaga } = this.props;
     const { requestValue } = this.state;
-    return submitHandlerBySaga(id, 'DELETE', `/api/eshs/v1/common/eshschemicalsafetyworkplace`, requestValue, this.getMaterialList);
+    return submitHandlerBySaga(id, 'DELETE', `/api/eshs/v1/common/eshschemicalsafetyspecialhealth`, requestValue, this.getMaterialList);
   };
 
   getMaterialList = () => {
@@ -222,7 +187,7 @@ class List extends React.Component {
       {
         key: 'materialList',
         type: 'GET',
-        url: '/api/eshs/v1/common/eshschemicalsafetyworkplace',
+        url: '/api/eshs/v1/common/eshschemicalsafetyspecialhealth',
       },
     ];
     getCallDataHandler(id, apiArr, this.handleResetClick);
@@ -235,7 +200,6 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: '',
-        SUB_CATEGORY: '',
         IS_APPLICATE: '',
         CONTENT_STANDARD: '0',
         SERIAL_NO: '',
@@ -248,7 +212,6 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: '',
-        SUB_CATEGORY: '',
         IS_APPLICATE: '',
         CONTENT_STANDARD: '0',
       },
@@ -262,9 +225,8 @@ class List extends React.Component {
   };
 
   setRequestValue = record => {
-    const { setSubMaterials, getSubCategories } = this;
+    const { setSubMaterials } = this;
     const { sagaKey: id, getCallDataHandler } = this.props;
-    getSubCategories(Number(record.CATEGORY));
     this.setState({
       requestValue: record,
       visible: false,
@@ -274,10 +236,9 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: record.CATEGORY,
-        SUB_CATEGORY: record.SUB_CATEGORY,
         IS_APPLICATE: 'Y',
         CONTENT_STANDARD: '0',
-        PARENT_ID: record.WORK_ID,
+        PARENT_ID: record.HEALTH_ID,
       },
     });
 
@@ -285,7 +246,7 @@ class List extends React.Component {
       {
         key: 'subMaterials',
         type: 'GET',
-        url: `/api/eshs/v1/common/eshschemicalsafetyworkplacesub?PARENT_ID=${record.WORK_ID}`,
+        url: `/api/eshs/v1/common/eshschemicalsafetyspecialhealthsub?PARENT_ID=${record.HEALTH_ID}`,
       },
     ];
     getCallDataHandler(id, apiArr, setSubMaterials);
@@ -309,7 +270,6 @@ class List extends React.Component {
           NAME_KOR: '',
           NAME_ENG: '',
           CATEGORY: prevState.requestValue.CATEGORY,
-          SUB_CATEGORY: prevState.requestValue.SUB_CATEGORY,
           IS_APPLICATE: 'Y',
           CONTENT_STANDARD: '0',
           PARENT_ID: '',
@@ -362,26 +322,6 @@ class List extends React.Component {
       },
     },
     {
-      title: '함량기준',
-      dataIndex: 'CONTENT_STANDARD',
-      key: 'CONTENT_STANDARD',
-      align: 'center',
-      render: (text, record, index) => {
-        const { handleSubInputChange } = this;
-        const { subRequestValue } = this.state;
-        if (index === 0) {
-          return (
-            <AntdInputNumber
-              className="ant-input-number input-number-sm"
-              value={subRequestValue.CONTENT_STANDARD}
-              onChange={e => handleSubInputChange(e, 'NUMBER', 'CONTENT_STANDARD')}
-            />
-          );
-        }
-        return text;
-      },
-    },
-    {
       title: '해당여부',
       dataIndex: 'IS_APPLICATE',
       key: 'IS_APPLICATE',
@@ -405,6 +345,26 @@ class List extends React.Component {
           );
         }
         return text === 'Y' ? '해당' : '비해당';
+      },
+    },
+    {
+      title: '함량기준',
+      dataIndex: 'CONTENT_STANDARD',
+      key: 'CONTENT_STANDARD',
+      align: 'center',
+      render: (text, record, index) => {
+        const { handleSubInputChange } = this;
+        const { subRequestValue } = this.state;
+        if (index === 0) {
+          return (
+            <AntdInputNumber
+              className="ant-input-number input-number-sm"
+              value={subRequestValue.CONTENT_STANDARD}
+              onChange={e => handleSubInputChange(e, 'NUMBER', 'CONTENT_STANDARD')}
+            />
+          );
+        }
+        return text;
       },
     },
     {
@@ -455,7 +415,6 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: prevState.requestValue.CATEGORY,
-        SUB_CATEGORY: prevState.requestValue.SUB_CATEGORY,
         IS_APPLICATE: 'Y',
         CONTENT_STANDARD: '0',
         PARENT_ID: '',
@@ -473,7 +432,6 @@ class List extends React.Component {
           NAME_KOR: '',
           NAME_ENG: '',
           CATEGORY: prevState.requestValue.CATEGORY,
-          SUB_CATEGORY: prevState.requestValue.SUB_CATEGORY,
           IS_APPLICATE: 'Y',
           CONTENT_STANDARD: '0',
           PARENT_ID: '',
@@ -493,7 +451,6 @@ class List extends React.Component {
           NAME_KOR: '',
           NAME_ENG: '',
           CATEGORY: prevState.requestValue.CATEGORY,
-          SUB_CATEGORY: prevState.requestValue.SUB_CATEGORY,
           IS_APPLICATE: 'Y',
           CONTENT_STANDARD: '0',
           PARENT_ID: '',
@@ -511,7 +468,6 @@ class List extends React.Component {
         NAME_KOR: '',
         NAME_ENG: '',
         CATEGORY: prevState.requestValue.CATEGORY,
-        SUB_CATEGORY: prevState.requestValue.SUB_CATEGORY,
         IS_APPLICATE: 'Y',
         CONTENT_STANDARD: '0',
         PARENT_ID: '',
@@ -566,7 +522,7 @@ class List extends React.Component {
       handleSubMaterialRowClick,
     } = this;
     const { columns, modalColumns } = this;
-    const { requestValue, visible, deleteConfirmMessage, categories, subCategories, isModified, dataSource, subTableVisible } = this.state;
+    const { requestValue, visible, deleteConfirmMessage, categories, isModified, dataSource, subTableVisible } = this.state;
     const { sagaKey, getCallDataHandler, result, changeFormData, formData } = this.props;
     return (
       <>
@@ -644,40 +600,22 @@ class List extends React.Component {
                         // min={MAX_SERIAL_NO}
                       />
                     </td>
-                    <th>분류</th>
+                    <th>CAS NO.</th>
                     <td>
+                      <AntdInput className="ant-input-sm" name="CAS_NO" value={requestValue.CAS_NO} onChange={e => handleInputChange(e, 'INPUT')} />
+                    </td>
+                    <th>분류</th>
+                    <td colSpan={3}>
                       <AntdSelect
                         className="select-sm"
                         onChange={e => handleInputChange(e, 'SELECT', 'CATEGORY')}
                         value={requestValue.CATEGORY ? Number(requestValue.CATEGORY) : ''}
-                        style={{ width: '100%' }}
+                        style={{ width: '170px' }}
                       >
                         {categories.map(item => (
                           <Select.Option value={item.NODE_ID}>{item.NAME_KOR}</Select.Option>
                         ))}
                       </AntdSelect>
-                    </td>
-                    <th>하위 분류</th>
-                    <td>
-                      <AntdSelect
-                        className="select-sm"
-                        onChange={e => handleInputChange(e, 'SELECT', 'SUB_CATEGORY')}
-                        value={requestValue.SUB_CATEGORY ? Number(requestValue.SUB_CATEGORY) : ''}
-                        style={{ width: '100%' }}
-                        disabled={requestValue.CATEGORY === 1958}
-                      >
-                        {subCategories.map(item => (
-                          <Select.Option value={item.NODE_ID}>{item.NAME_KOR}</Select.Option>
-                        ))}
-                      </AntdSelect>
-                    </td>
-                    <th>함량기준</th>
-                    <td>
-                      <AntdInputNumber
-                        className="ant-input-number input-number-sm"
-                        value={requestValue.CONTENT_STANDARD}
-                        onChange={e => handleInputChange(e, 'NUMBER', 'CONTENT_STANDARD')}
-                      />
                     </td>
                   </tr>
                   <tr>
@@ -688,10 +626,6 @@ class List extends React.Component {
                     <th>화학물질명_영문</th>
                     <td>
                       <AntdInput className="ant-input-sm" name="NAME_ENG" value={requestValue.NAME_ENG} onChange={e => handleInputChange(e, 'INPUT')} />
-                    </td>
-                    <th>CAS NO.</th>
-                    <td>
-                      <AntdInput className="ant-input-sm" name="CAS_NO" value={requestValue.CAS_NO} onChange={e => handleInputChange(e, 'INPUT')} />
                     </td>
                     <th>해당여부</th>
                     <td>
@@ -705,6 +639,14 @@ class List extends React.Component {
                         <Select.Option value="Y">해당</Select.Option>
                         <Select.Option value="N">비해당</Select.Option>
                       </AntdSelect>
+                    </td>
+                    <th>함량기준</th>
+                    <td>
+                      <AntdInputNumber
+                        className="ant-input-number input-number-sm"
+                        value={requestValue.CONTENT_STANDARD}
+                        onChange={e => handleInputChange(e, 'NUMBER', 'CONTENT_STANDARD')}
+                      />
                     </td>
                   </tr>
                 </tbody>
@@ -730,7 +672,7 @@ class List extends React.Component {
           getCallDataHandler={getCallDataHandler}
           result={result}
           setRequestValue={setRequestValue}
-          apiUrl="/api/eshs/v1/common/eshschemicalsafetyworkplace"
+          apiUrl="/api/eshs/v1/common/eshschemicalsafetyspecialhealth"
           tableColumns={modalColumns}
           SearchComp={SearchComp}
           changeFormData={changeFormData}
