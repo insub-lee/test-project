86dd63147eb677a35f874aee39d76752d5cdbef6
LeeInSub <esub17@naver.com>
2020-04-23 12:24:32 +0900

    EshQual cond  comp, config 추가

---
+++ src/components/BizBuilder/Field/EshsCondConfig.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import * as PropTypes from 'prop-types';
+import { Input, Select, Radio } from 'antd';
+import { debounce } from 'lodash';
+
+const { Option } = Select;
+class EshsCondConfig extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleChangeViewCompData = debounce(this.handleChangeViewCompData, 500);
+  }
+
+  handleChangeViewCompData = (key, value) => {
+    const { changeViewCompData, groupIndex, rowIndex, colIndex, configInfo } = this.props;
+    configInfo.property[key] = value;
+    changeViewCompData(groupIndex, rowIndex, colIndex, 'CONFIG', configInfo);
+  };
+
+  render() {
+    const { configInfo } = this.props;
+
+    return (
+      <>
+        <div className="popoverItem popoverItemInput">
+          <span className="spanLabel">Cond dataKey 설정</span>
+          <Input
+            style={{ width: '100%' }}
+            defaultValue={(configInfo && configInfo.property && configInfo.property.COND_KEY) || ''}
+            onChange={e => this.handleChangeViewCompData('COND_KEY', e.target.value)}
+          />
+        </div>
+        <div className="popoverItem popoverItemInput">
+          <span className="spanLabel">Cond Columns 설정</span>
+          <Select
+            defaultValue={(configInfo && configInfo.property && configInfo.property.COND_COLUMN) || ''}
+            style={{ width: '100%' }}
+            onChange={value => this.handleChangeViewCompData('COND_COLUMN', value)}
+          >
+            <Option value="approve">Step 1</Option>
+            <Option value="improve">Step 2</Option>
+            <Option value="result">Step 3 [Result]</Option>
+            <Option value="confirm">Step 3 [Confirm]</Option>
+          </Select>
+        </div>
+        <div className="popoverItem popoverItemInput">
+          <span className="spanLabel">Cond viewType</span>
+          <Select
+            defaultValue={(configInfo && configInfo.property && configInfo.property.COND_TYPE) || ''}
+            style={{ width: '100%' }}
+            onChange={value => this.handleChangeViewCompData('COND_TYPE', value)}
+          >
+            <Option value="INPUT">INPUT</Option>
+            <Option value="VIEW">VIEW</Option>
+          </Select>
+        </div>
+        <div className="popoverItem popoverItemInput">
+          <span className="spanLabel">Cond Step 설정</span>
+          <Select
+            defaultValue={(configInfo && configInfo.property && configInfo.property.STEP) || ''}
+            style={{ width: '100%' }}
+            onChange={value => this.handleChangeViewCompData('STEP', value)}
+          >
+            <Option value="1">1</Option>
+            <Option value="2">2</Option>
+            <Option value="3">3</Option>
+          </Select>
+        </div>
+        <div className="popoverItem popoverItemInput">
+          <span className="spanLabel">리스트 Default값</span>
+          <Radio.Group
+            onChange={e => this.handleChangeViewCompData('DEFAULT_LIST', e.target.value)}
+            value={(configInfo && configInfo.property && configInfo.property.DEFAULT_LIST) || 'N'}
+          >
+            <Radio value="N">미사용</Radio>
+            <Radio value="Y">사용</Radio>
+          </Radio.Group>
+        </div>
+        <div className="popoverItem popoverItemInput">
+          <span className="spanLabel">리스트 추가 버튼</span>
+          <Radio.Group
+            onChange={e => this.handleChangeViewCompData('BTN_FLAG', e.target.value)}
+            value={(configInfo && configInfo.property && configInfo.property.BTN_FLAG) || 'N'}
+          >
+            <Radio value="N">미사용</Radio>
+            <Radio value="Y">사용</Radio>
+          </Radio.Group>
+        </div>
+      </>
+    );
+  }
+}
+
+EshsCondConfig.propTypes = {
+  configInfo: PropTypes.any,
+  changeViewCompData: PropTypes.func,
+  groupIndex: PropTypes.number,
+  rowIndex: PropTypes.number,
+  colIndex: PropTypes.number,
+};
+
+EshsCondConfig.defaultProps = {};
+
+export default EshsCondConfig;
