6d1e220ffb49c9ee5e68c7082b8e7f9d3d74474a
LeeInSub <esub17@naver.com>
2020-04-23 22:01:22 +0900

    EshsQual 승인. 확인결과 초기값 수정

--- src/apps/eshs/user/qualSqtb/sqConfirmResult/pages/CustomModify/index.js
+++ src/apps/eshs/user/qualSqtb/sqConfirmResult/pages/CustomModify/index.js
@@ -28,32 +28,11 @@ class ModifyPage extends Component {
   }
 
   componentDidMount() {
-    const { sagaKey: id, formData, getExtraApiData } = this.props;
-    const USER_ID = (formData && formData.REG_USER_ID) || 0;
-    const apiArray = [
-      {
-        key: 'info',
-        url: '/api/eshs/v1/common/userinfowithgender',
-        type: 'POST',
-        params: { PARAM: { USER_ID } },
-      },
-    ];
+    const { sagaKey: id, formData, changeFormData } = this.props;
+    const EXAM_DT = (formData && formData.EXAM_DT) || '';
 
-    getExtraApiData(id, apiArray, this.appStart);
+    if (!EXAM_DT) changeFormData(id, 'EXAM_DT', moment(new Date()).format('YYYY-MM-DD'));
   }
-
-  appStart = sagaKey => {
-    const { setFormData, formData, extraApiData } = this.props;
-    const userInfo = (extraApiData && extraApiData.info && extraApiData.info.userInfo) || {};
-    setFormData(sagaKey, {
-      ...formData,
-      REQ_EMP_NO: userInfo.EMP_NO,
-      REQ_INTRA_PHONE: userInfo.OFFICE_TEL_NO,
-      REQ_EMP_NM: userInfo.NAME,
-      REQ_DEPT_NM: userInfo.DEPT,
-      EXAM_DT: moment(new Date()).format('YYYY-MM-DD'),
-    });
-  };
 
   static getDerivedStateFromProps(nextProps, prevState) {
     const {
@@ -290,6 +269,7 @@ ModifyPage.propTypes = {
   extraApiData: PropTypes.any,
   setFormData: PropTypes.func,
   modifyTask: PropTypes.func,
+  sagaKey: PropTypes.string,
 };
 
 ModifyPage.defaultProps = {
