fa7bc6060cb0c44eedf243a2c1733f479744f05b
김준혁 <jhkim3@kb-sys.co.kr>
2020-05-06 19:19:56 +0900

    빌더 베이스: node multi view, node by code view 필드 컴포넌트 추가/RadioCodeValueAsCharComp didmount 조회 submitExtraHandler로 변경

--- src/components/BizBuilder/Field/RadioCodeValueAsCharComp.js
+++ src/components/BizBuilder/Field/RadioCodeValueAsCharComp.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 import { Radio } from 'antd';
 
 class RadioCodeValueAsCharComp extends Component {
-  state = {
-    dataSource: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+    };
+  }
 
   componentDidMount() {
     const {
-      getExtraApiData,
+      // getExtraApiData,
       sagaKey: id,
       COMP_FIELD,
       CONFIG: {
         property: { mapId },
       },
+      submitExtraHandler,
     } = this.props;
-    const apiArys = [{ key: `${COMP_FIELD}`, url: `/api/admin/v1/common/categoryMapList?MAP_ID=${mapId}`, type: 'GET' }];
-    getExtraApiData(id, apiArys, this.initDataBind);
+    // const apiArys = [{ key: `${COMP_FIELD}`, url: `/api/admin/v1/common/categoryMapList?MAP_ID=${mapId}`, type: 'GET' }];
+    const url = `/api/admin/v1/common/categoryMapList?MAP_ID=${mapId}`;
+    // getExtraApiData(id, apiArys, this.initDataBind);
+    submitExtraHandler(id, 'GET', url, {}, this.initData);
   }
 
   initDataBind = sagaKey => {
@@ -26,18 +32,30 @@ class RadioCodeValueAsCharComp extends Component {
     this.setState({ dataSource });
   };
 
+  initData = (id, response) => {
+    const { categoryMapList } = response;
+    if (categoryMapList && categoryMapList.length > 0) {
+      const dataSource = categoryMapList.filter(f => f.USE_YN === 'Y' && f.LVL > 0);
+      this.setState({ dataSource });
+    }
+  };
+
   onChangeRadio = e => {
     const { sagaKey, COMP_FIELD, changeFormData } = this.props;
     changeFormData(sagaKey, COMP_FIELD, e.target.value);
   };
 
   render() {
-    console.debug(this.props, this.state);
-    const { colData } = this.props;
+    const { colData, readOnly, CONFIG } = this.props;
     const { dataSource } = this.state;
     return (
-      <Radio.Group onChange={this.onChangeRadio} value={colData}>
-        {dataSource && dataSource.map(rdo => <Radio value={rdo.CODE}>{rdo.NAME_KOR}</Radio>)}
+      <Radio.Group onChange={this.onChangeRadio} value={colData} disabled={readOnly || CONFIG.property.readOnly}>
+        {dataSource &&
+          dataSource.map(rdo => (
+            <Radio key={`RadioCodeValueAsCharComp-rdo.CODE-${rdo.CODE}`} value={rdo.CODE}>
+              {rdo.NAME_KOR}
+            </Radio>
+          ))}
       </Radio.Group>
     );
   }
