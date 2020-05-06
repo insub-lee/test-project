0d657d1506675793b991b9fcf80487cf8c76d9e3
hsso <hsso@kb-sys.co.kr>
2020-05-06 21:39:05 +0900

    화학물질관리 산업안전보건법(DB) 관리페이지 추가

---
+++ src/apps/eshs/user/environment/chemicalMaterialManagement/manage/safetyAct/List/columnDefs.js
@@ -0,0 +1,38 @@
+export const columnDefs = [
+  {
+    headerName: 'CAS_NO',
+    field: 'CAS_NO',
+  },
+  {
+    headerName: '물질명',
+    field: 'NAME_KOR',
+  },
+  {
+    headerName: '노출기준설정물질',
+    field: 'IS_EXPOSURE',
+  },
+  {
+    headerName: '허가대상물질',
+    field: 'IS_ALLOW',
+  },
+  {
+    headerName: '허용기준설정물질(제한물질)',
+    field: 'IS_PERMISSION',
+  },
+  {
+    headerName: '금지물질',
+    field: 'IS_BAN',
+  },
+  {
+    headerName: '작업환경측정대상물질',
+    field: 'IS_WORKSPACE',
+  },
+  {
+    headerName: '관리대상물질',
+    field: 'IS_MANAGED',
+  },
+  {
+    headerName: '특별관리대상물질',
+    field: 'IS_SPE_MANAGED',
+  },
+];
