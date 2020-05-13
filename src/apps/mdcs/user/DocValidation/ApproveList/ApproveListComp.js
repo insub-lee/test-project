fcea654913b1f5651c05b8737671228e84ec3380
lesm74 <sangmin.biz@gmail.com>
2020-05-13 11:22:41 +0900

    유효성 점검 추가 개발

--- src/apps/mdcs/user/DocValidation/ApproveList/ApproveListComp.js
+++ src/apps/mdcs/user/DocValidation/ApproveList/ApproveListComp.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Icon, Table, Modal, message } from 'antd';
-import { fromJS } from 'immutable';
 import moment from 'moment';
 import StyledLineTable from 'commonStyled/MdcsStyled/Table/StyledLineTable';
 import ContentsWrapper from 'commonStyled/MdcsStyled/Wrapper/ContentsWrapper';
 import StyledContentsModal from 'commonStyled/MdcsStyled/Modal/StyledContentsModal';
 import StyledButton from 'commonStyled/Buttons/StyledButton';
 import BizBuilderBase from 'components/BizBuilderBase';
-
-import ApproveView from './approveView';
-
 const AntdLineTable = StyledLineTable(Table);
 const AntdModal = StyledContentsModal(Modal);
-class ApproveReqListComp extends Component {
+class ApproveListComp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      list: [],
       visible: false,
       workSeq: undefined,
       taskSeq: undefined,
@@ -78,14 +75,29 @@ class ApproveReqListComp extends Component {
   ];
 
   componentDidMount() {
-    const { getCustomDataBind } = this.props;
-    const rtnUrl = '/api/workflow/v1/common/approve/ValidateReqListHandler';
-    getCustomDataBind('POST', rtnUrl, {});
+    const { id, submitHandlerBySaga } = this.props;
+    const rtnUrl = '/api/workflow/v1/common/approve/ValidateCompleteListHandler';
+    submitHandlerBySaga(id, 'POST', rtnUrl, { PARAM: { REL_TYPE: 2 } }, this.initData);
   }
 
+  initData = (id, response) => {
+    const { list } = response;
+    this.setState({ list });
+  };
+
   onRowClick = (record, rowIndex, e) => {
-    this.props.setSelectedRow(record);
     this.setState({ visible: true, workSeq: record.WORK_SEQ, taskSeq: record.TASK_SEQ, taskOrginSeq: record.TASK_ORIGIN_SEQ, title: record.TITLE });
+  };
+
+  clickCoverView = (workSeq, taskSeq, viewMetaSeq) => {
+    const coverView = { workSeq, taskSeq, viewMetaSeq, visible: true, viewType: 'VIEW' };
+    this.setState({ coverView });
+  };
+
+  onCloseCoverView = () => {
+    const { coverView } = this.state;
+    const tempCoverView = { ...coverView, visible: false };
+    this.setState({ coverView: tempCoverView });
   };
 
   onModalClose = () => {
@@ -93,18 +105,17 @@ class ApproveReqListComp extends Component {
   };
 
   render() {
-    const { workSeq, taskSeq, visible, coverView } = this.state;
-    const { customDataList } = this.props;
+    const { list, workSeq, taskSeq, visible, coverView } = this.state;
     return (
       <ContentsWrapper>
         <div className="pageTitle">
           <p>
-            <Icon type="form" /> 결재요청 LIST
+            <Icon type="form" /> 결재완료 LIST
           </p>
         </div>
         <AntdLineTable
           columns={this.getTableColumns()}
-          dataSource={customDataList}
+          dataSource={list}
           onRow={(record, rowIndex) => ({
             onClick: e => this.onRowClick(record, rowIndex, e),
           })}
@@ -112,11 +123,44 @@ class ApproveReqListComp extends Component {
           className="tableWrapper"
         />
         <AntdModal title="유효성 점검" visible={visible} width={680} destroyOnClose onCancel={this.onModalClose} footer={[]}>
-          <ApproveView {...this.props} WORK_SEQ={workSeq} TASK_SEQ={taskSeq} onValidateProcess={this.onValidateProcess} onModalClose={this.onModalClose} />
+          <BizBuilderBase
+            sagaKey="approveReqView"
+            workSeq={workSeq}
+            taskSeq={taskSeq}
+            viewType="VIEW"
+            clickCoverView={this.clickCoverView}
+            ViewCustomButtons={() => false}
+          />
+        </AntdModal>
+        <AntdModal
+          className="modalWrapper modalTechDoc modalCustom"
+          title="표지 보기"
+          width={800}
+          destroyOnClose
+          visible={coverView.visible}
+          onCancel={this.onCloseCoverView}
+          footer={[]}
+        >
+          <BizBuilderBase
+            sagaKey="CoverView"
+            viewType={coverView.viewType}
+            workSeq={coverView.workSeq}
+            taskSeq={coverView.taskSeq}
+            viewMetaSeq={coverView.viewMetaSeq}
+            onCloseCoverView={this.onCloseCoverView}
+            onCloseModalHandler={this.onClickModifyDoCoverView}
+            ViewCustomButtons={({ onCloseCoverView }) => (
+              <div style={{ textAlign: 'center', marginTop: '12px' }}>
+                <StyledButton className="btn-primary" onClick={onCloseCoverView}>
+                  닫기
+                </StyledButton>
+              </div>
+            )}
+          />
         </AntdModal>
       </ContentsWrapper>
     );
   }
 }
 
-export default ApproveReqListComp;
+export default ApproveListComp;
---
+++ src/apps/mdcs/user/DocValidation/ApproveList/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ApproveBase from 'apps/Workflow/components/ApproveBase';
+
+import ApproveListComp from './ApproveListComp';
+
+const ApproveList = () => <ApproveBase id="CheckList" component={ApproveListComp} />;
+
+export default ApproveList;
