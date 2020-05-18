ba39ad36acbfec943ba9c899782576983137796c
LeeInSub <esub17@naver.com>
2020-05-18 22:39:45 +0900

    EshsQual 신청목록 list 화면에서 신청/승인 클릭시 modal 추가

--- src/apps/eshs/user/safety/eshsQual/qualApprove/applyList/index.js
+++ src/apps/eshs/user/safety/eshsQual/qualApprove/applyList/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import BizBuilderBase from 'components/BizBuilderBase';
+import CustomList from 'apps/eshs/user/safety/eshsQual/qualApprove/applyList/pages/CustomList';
 
+import ConfirmRequest from 'apps/eshs/user/safety/eshsQual/qualApply/confirmRequest'; // 신청
+import ConfirmReusltView from 'apps/eshs/user/safety/eshsQual/qualApply/improveConfirmView'; // 개선결과조회
+import InterLockRequest from 'apps/eshs/user/safety/eshsQual/qualApply/interLockRequest'; // InterLock 해제신청
+import InterLockResult from 'apps/eshs/user/safety/eshsQual/qualApprove/interLockResult'; // InterLock 해제결과
 class ApplyList extends Component {
   constructor(props) {
     super(props);
@@ -25,11 +30,16 @@ class ApplyList extends Component {
         <BizBuilderBase
           sagaKey={sagaKey}
           workSeq={6821}
+          CustomListPage={CustomList}
           listMetaSeq={8041}
           viewType="LIST"
           loadingComplete={this.loadingComplete}
           InputCustomButtons={() => null}
           ModifyCustomButtons={() => null}
+          ConfirmRequest={(taskSeq, viewType) => <ConfirmRequest taskSeq={taskSeq} viewType={viewType} />}
+          ConfirmResult={taskSeq => <ConfirmReusltView taskSeq={taskSeq} />}
+          InterLockRequest={(taskSeq, viewType) => <InterLockRequest taskSeq={taskSeq} viewType={viewType} />}
+          InterLockResult={taskSeq => <InterLockResult taskSeq={taskSeq} />}
         />
       </>
     );
