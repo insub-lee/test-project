6da50a2371c394fbb022f1c6f1eb3bcf6c9acca5
hyoseong3291 <wjdgytjd3291@gmail.com>
2020-06-08 22:42:06 +0900

    Eshs > 안전지킴이 > 위험성평가 > 안전보건상 위험정보 개발중... 추가 소스

--- src/apps/eshs/admin/safety/Danger/dangerInfo/Modify/View.js
+++ src/apps/eshs/admin/safety/Danger/dangerInfo/Modify/View.js
@@ -53,7 +53,7 @@ const tagArray = (onChangeData, formData) => [
         className="ant-input-number-sm ant-input-number-inline"
         onChange={value => onChangeData(`JAEHAE`, value)}
         style={{ width: 100 }}
-        value={formData.JAEHAE}
+        value={formData.INFO_DATA ? formData.INFO_DATA.JAEHAE : formData.JAEHAE}
       />
       건
     </td>
@@ -65,7 +65,7 @@ const tagArray = (onChangeData, formData) => [
         className="ant-input-number-sm ant-input-number-inline"
         onChange={value => onChangeData(`ACHA`, value)}
         style={{ width: 100 }}
-        value={formData.ACHA}
+        value={formData.INFO_DATA ? formData.INFO_DATA.ACHA : formData.ACHA}
       />
       건
     </td>
@@ -73,32 +73,38 @@ const tagArray = (onChangeData, formData) => [
   <th colSpan={3}>근로자 구성 및 경력특성</th>,
   <>
     <td colSpan={3} rowSpan={1}>
-      <Checkbox value={formData.EMP_WOMAN} onChange={e => onChangeData(`EMP_WOMAN`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.EMP_WOMAN : formData.EMP_WOMAN} onChange={e => onChangeData(`EMP_WOMAN`, e.target.checked)}>
         여성근로자
       </Checkbox>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <Checkbox value={formData.EMP_NEW} onChange={e => onChangeData(`EMP_NEW`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.EMP_NEW : formData.EMP_NEW} onChange={e => onChangeData(`EMP_NEW`, e.target.checked)}>
         1년 미만 미숙련자
       </Checkbox>
     </td>
   </>,
   <>
     <td colSpan={3} rowSpan={1}>
-      <Checkbox value={formData.EMP_OLD} onChange={e => onChangeData(`EMP_OLD`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.EMP_OLD : formData.EMP_OLD} onChange={e => onChangeData(`EMP_OLD`, e.target.checked)}>
         고령근로자
       </Checkbox>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <Checkbox value={formData.EMP_TEMP} onChange={e => onChangeData(`EMP_TEMP`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.EMP_TEMP : formData.EMP_TEMP} onChange={e => onChangeData(`EMP_TEMP`, e.target.checked)}>
         비정규직 근로자
       </Checkbox>
     </td>
   </>,
   <>
     <td colSpan={3} rowSpan={1}>
-      <Checkbox value={formData.EMP_FOREIGN} onChange={e => onChangeData(`EMP_FOREIGN`, e.target.checked)}>
+      <Checkbox
+        value={formData.INFO_DATA ? formData.INFO_DATA.EMP_FOREIGN : formData.EMP_FOREIGN}
+        onChange={e => onChangeData(`EMP_FOREIGN`, e.target.checked)}
+      >
         외국인 근로자
       </Checkbox>
-      <Checkbox value={formData.EMP_DISABLE} onChange={e => onChangeData(`EMP_DISABLE`, e.target.checked)}>
+      <Checkbox
+        value={formData.INFO_DATA ? formData.INFO_DATA.EMP_DISABLE : formData.EMP_DISABLE}
+        onChange={e => onChangeData(`EMP_DISABLE`, e.target.checked)}
+      >
         장애 근로자
       </Checkbox>
     </td>
@@ -106,10 +112,10 @@ const tagArray = (onChangeData, formData) => [
   <>
     <th>교대작업 유무</th>
     <td colSpan={2}>
-      <Checkbox value={formData.WORK_Y} onChange={e => onChangeData(`WORK_Y`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.WORK_Y : formData.WORK_Y} onChange={e => onChangeData(`WORK_Y`, e.target.checked)}>
         유
       </Checkbox>
-      <Checkbox value={formData.WORK_N} onChange={e => onChangeData(`WORK_N`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.WORK_N : formData.WORK_N} onChange={e => onChangeData(`WORK_N`, e.target.checked)}>
         무
       </Checkbox>
     </td>
@@ -117,10 +123,10 @@ const tagArray = (onChangeData, formData) => [
   <>
     <th>운반수단 유무</th>
     <td colSpan={2}>
-      <Checkbox value={formData.MOVE_Y} onChange={e => onChangeData(`MOVE_Y`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.MOVE_Y : formData.MOVE_Y} onChange={e => onChangeData(`MOVE_Y`, e.target.checked)}>
         기계
       </Checkbox>
-      <Checkbox value={formData.MOVE_N} onChange={e => onChangeData(`MOVE_N`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.MOVE_N : formData.MOVE_N} onChange={e => onChangeData(`MOVE_N`, e.target.checked)}>
         인력
       </Checkbox>
     </td>
@@ -128,10 +134,10 @@ const tagArray = (onChangeData, formData) => [
   <>
     <th>안전작업허가 필요작업 유무</th>
     <td colSpan={2}>
-      <Checkbox value={formData.AGREE_Y} onChange={e => onChangeData(`AGREE_Y`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.AGREE_Y : formData.AGREE_Y} onChange={e => onChangeData(`AGREE_Y`, e.target.checked)}>
         유
       </Checkbox>
-      <Checkbox value={formData.AGREE_N} onChange={e => onChangeData(`AGREE_N`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.AGREE_N : formData.AGREE_N} onChange={e => onChangeData(`AGREE_N`, e.target.checked)}>
         무
       </Checkbox>
     </td>
@@ -139,17 +145,28 @@ const tagArray = (onChangeData, formData) => [
   <>
     <th rowSpan={2}>중량물 인력취급시 단위중량( kg예상) 및 형태</th>
     <td colSpan={2} rowSpan={2}>
-      <AntdInputNumber className="ant-input-number-sm" style={{ width: 100 }} onChange={value => onChangeData(`WEIGHT_KG`, value)} value={formData.WEIGHT_KG} />
-      <Checkbox value={formData.WEIGHT_UP} onChange={e => onChangeData(`WEIGHT_UP`, e.target.checked)}>
+      <AntdInputNumber
+        className="ant-input-number-sm"
+        style={{ width: 100 }}
+        onChange={value => onChangeData(`WEIGHT_KG`, value)}
+        value={formData.INFO_DATA ? formData.INFO_DATA.WEIGHT_KG : formData.WEIGHT_KG}
+      />
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.WEIGHT_UP : formData.WEIGHT_UP} onChange={e => onChangeData(`WEIGHT_UP`, e.target.checked)}>
         들기
       </Checkbox>
-      <Checkbox value={formData.WEIGHT_PUSH} onChange={e => onChangeData(`WEIGHT_PUSH`, e.target.checked)}>
+      <Checkbox
+        value={formData.INFO_DATA ? formData.INFO_DATA.WEIGHT_PUSH : formData.WEIGHT_PUSH}
+        onChange={e => onChangeData(`WEIGHT_PUSH`, e.target.checked)}
+      >
         밀기
       </Checkbox>
-      <Checkbox value={formData.WEIGHT_PULL} onChange={e => onChangeData(`WEIGHT_PULL`, e.target.checked)}>
+      <Checkbox
+        value={formData.INFO_DATA ? formData.INFO_DATA.WEIGHT_PULL : formData.WEIGHT_PULL}
+        onChange={e => onChangeData(`WEIGHT_PULL`, e.target.checked)}
+      >
         끌기
       </Checkbox>
-      <Checkbox value={formData.WEIGHT_ETC} onChange={e => onChangeData(`WEIGHT_ETC`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.WEIGHT_ETC : formData.WEIGHT_ETC} onChange={e => onChangeData(`WEIGHT_ETC`, e.target.checked)}>
         기타
       </Checkbox>
     </td>
@@ -158,13 +175,13 @@ const tagArray = (onChangeData, formData) => [
   <>
     <th>작업환경측정 측정유무</th>
     <td colSpan={2}>
-      <Checkbox value={formData.ENV_Y} onChange={e => onChangeData(`ENV_Y`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.ENV_Y : formData.ENV_Y} onChange={e => onChangeData(`ENV_Y`, e.target.checked)}>
         측정
       </Checkbox>
-      <Checkbox value={formData.ENV_N} onChange={e => onChangeData(`ENV_N`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.ENV_N : formData.ENV_N} onChange={e => onChangeData(`ENV_N`, e.target.checked)}>
         미측정
       </Checkbox>
-      <Checkbox value={formData.ENV_Z} onChange={e => onChangeData(`ENV_Z`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.ENV_Z : formData.ENV_Z} onChange={e => onChangeData(`ENV_Z`, e.target.checked)}>
         해당무
       </Checkbox>
     </td>
@@ -176,7 +193,7 @@ const tagArray = (onChangeData, formData) => [
         className="ant-input-number-sm ant-input-number-inline"
         style={{ width: 100 }}
         onChange={value => onChangeData(`ENV_NOISE`, value)}
-        value={formData.ENV_NOISE}
+        value={formData.INFO_DATA ? formData.INFO_DATA.ENV_NOISE : formData.ENV_NOISE}
       />
       dB
     </td>
@@ -184,10 +201,10 @@ const tagArray = (onChangeData, formData) => [
   <>
     <th>작업에 대한 특별안전보건교육 필요유무</th>
     <td colSpan={2}>
-      <Checkbox value={formData.EDU_Y} onChange={e => onChangeData(`EDU_Y`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.EDU_Y : formData.EDU_Y} onChange={e => onChangeData(`EDU_Y`, e.target.checked)}>
         유
       </Checkbox>
-      <Checkbox value={formData.EDU_N} onChange={e => onChangeData(`EDU_N`, e.target.checked)}>
+      <Checkbox value={formData.INFO_DATA ? formData.INFO_DATA.EDU_N : formData.EDU_N} onChange={e => onChangeData(`EDU_N`, e.target.checked)}>
         무
       </Checkbox>
     </td>
@@ -301,6 +318,7 @@ class View extends Component {
     });
     if (overlab && overlab.length !== 0) {
       setFormData(id, overlab);
+      changeFormData(id, 'INFO_DATA', JSON.parse(overlab.INFO_DATA));
     }
   };
 
@@ -338,10 +356,14 @@ class View extends Component {
 
   saveBeforeProcess = () => {
     const { formData, sagaKey: id, changeFormData, saveTask } = this.props;
-    const infoData = JSON.stringify(formData);
-    const callBackSave = changeFormDataCallBackSaveTask(infoData, id, changeFormData, saveTask);
-    callBackSave.next();
-    callBackSave.next();
+    if (formData.PROCESS_ID) {
+      const infoData = JSON.stringify(formData);
+      const callBackSave = changeFormDataCallBackSaveTask(infoData, id, changeFormData, saveTask);
+      callBackSave.next();
+      callBackSave.next();
+    } else {
+      message.warning('분류를 먼저 선택해주세요');
+    }
   };
 
   render() {
@@ -359,7 +381,6 @@ class View extends Component {
       selectedRowKeys,
       onChange: this.onSelectChangeModal,
     };
-    console.debug('list : ', listData);
     return (
       <StyledContentsWrapper>
         <StyledCustomSearchWrapper className="search-wrapper-inline">
@@ -386,9 +407,15 @@ class View extends Component {
               <StyledButton className="btn-primary btn-first btn-sm" onClick={() => message.info('개발 중입니다.')}>
                 엑셀받기
               </StyledButton>
-              <StyledButton className="btn-primary btn-first btn-sm" onClick={this.saveBeforeProcess}>
-                저장/수정
-              </StyledButton>
+              {formData.TASK_SEQ ? (
+                <StyledButton className="btn-primary btn-first btn-sm" onClick={this.saveBeforeProcess}>
+                  수정
+                </StyledButton>
+              ) : (
+                <StyledButton className="btn-primary btn-first btn-sm" onClick={this.saveBeforeProcess}>
+                  저장
+                </StyledButton>
+              )}
             </StyledButtonWrapper>
           </div>
         </StyledCustomSearchWrapper>
@@ -454,7 +481,7 @@ class View extends Component {
                       <AntdSelect
                         className="select-sm"
                         style={{ width: '100%' }}
-                        defaultValue={formData[`M_CD${index + 1}`]}
+                        value={formData.INFO_DATA ? formData.INFO_DATA[`M_CD${index + 1}`] : formData[`M_CD${index + 1}`]}
                         onChange={value => this.onChangeData(`M_CD${index + 1}`, value)}
                       >
                         {selectData && selectData.map(item => <Option value={item.NODE_ID}>{item.NAME_KOR}</Option>)}
@@ -464,15 +491,20 @@ class View extends Component {
                       <AntdInputNumber
                         className="ant-input-number-sm"
                         style={{ width: 100 }}
-                        defaultValue={formData[`M_QTY${index + 1}`]}
+                        defaultValue={formData.INFO_DATA && formData.INFO_DATA[`M_QTY${index + 1}`]}
                         onChange={value => this.onChangeData(`M_QTY${index + 1}`, value)}
                       />
                     </td>
                     <td align="center">
-                      <AntdSearchInput className="input-search-sm" value={formData[`C_NAME${index + 1}`]} readOnly onClick={this.onChangeModal} />
+                      <AntdSearchInput
+                        className="input-search-sm"
+                        value={formData.INFO_DATA && formData.INFO_DATA[`C_NAME${index + 1}`]}
+                        readOnly
+                        onClick={this.onChangeModal}
+                      />
                     </td>
                     <td align="center">
-                      <span>{formData[`C_GR${index + 1}`] || ''}</span>
+                      <span>{(formData.INFO_DATA && formData.INFO_DATA[`C_GR${index + 1}`]) || ''}</span>
                     </td>
                     {tag}
                   </tr>
@@ -539,10 +571,14 @@ View.propTypes = {
   sagaKey: PropTypes.string,
   extraApiData: PropTypes.object,
   formData: PropTypes.object,
+  listData: PropTypes.object,
   getExtraApiData: PropTypes.func,
   changeFormData: PropTypes.func,
   saveTask: PropTypes.func,
   modalColumns: PropTypes.array,
+  getListData: PropTypes.func,
+  changeSearchData: PropTypes.func,
+  setFormData: PropTypes.func,
 };
 
 View.defaultProps = {
