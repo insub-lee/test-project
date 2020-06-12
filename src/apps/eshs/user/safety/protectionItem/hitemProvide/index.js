77d9941f8d3288a2cdeb44cfee1f582b9e40f4fd
hsso <hsso@kb-sys.co.kr>
2020-06-12 16:16:22 +0900

    오타 수정, 부서 선택 수정 중

---
+++ src/apps/eshs/user/safety/protectionItem/hitemProvide/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import BizMicroDevBase from 'components/BizMicroDevBase';
+import List from './list';
+
+const HitemProvide = () => <BizMicroDevBase sagakey="HitemProvide" component={List} />;
+
+export default HitemProvide;
