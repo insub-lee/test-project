1f27d5ad202891b27253ef7995375aa2b3808203
LeeInSub <esub17@naver.com>
2020-06-17 20:15:27 +0900

    TableName 수정

--- src/apps/eshs/user/environment/eia/eiWater/index.js
+++ src/apps/eshs/user/environment/eia/eiWater/index.js
@@ -4,7 +4,7 @@ import MainPage from './MainPage';
 
 class eiWater extends Component {
   render() {
-    return <BizMicroDevBase component={MainPage} sagaKey="eiWater" tbName="WATER" />;
+    return <BizMicroDevBase component={MainPage} sagaKey="eiWater" tbName="WATER_ITEM" />;
   }
 }
 
