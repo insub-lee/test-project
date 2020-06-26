c9df2e436a761b0c596f32cc0ce9ed026c2f8df9
jdlee <jdlee@kb-sys.co.kr>
2020-06-26 17:35:55 +0900

    임시 업로드

--- src/containers/portal/App/FileManage/constants.js
+++ src/containers/portal/App/FileManage/constants.js
@@ -1,3 +1,29 @@
 const prefix = 'filemanage';
 export const GET_SITE_LIST = `${prefix}/GET_SITE_LIST`;
 export const SET_SITE_LIST = `${prefix}/SET_SITE_LIST`;
+export const GET_SYSFILE_LIST = `${prefix}/GET_SYSFILE_LIST`;
+export const SET_SYSFILE_LIST = `${prefix}/SET_SYSFILE_LIST`;
+
+export const GET_USER_FOLDER_TREE = `${prefix}/GET_USER_FOLDER_TREE`;
+export const SET_USER_FOLDER_TREE = `${prefix}/SET_USER_FOLDER_TREE`;
+export const GET_USER_FILE_LIST = `${prefix}/GET_USER_FILE_LIST`;
+export const SET_USER_FILE_LIST = `${prefix}/SET_USER_FILE_LIST`;
+
+export const INSERT_USER_FOLDER = `${prefix}/INSERT_USER_FOLDER`;
+export const UPDATE_USER_FOLDER = `${prefix}/UPDATE_USER_FOLDER`;
+export const DELETE_USER_FOLDER = `${prefix}/DELETE_USER_FOLDER`;
+
+export const UPLOAD_FILE = `${prefix}/UPLOAD_FILE`;
+export const DELETE_FILE = `${prefix}/DELETE_FILE`;
+
+export const GET_FILE_AUTH = `${prefix}/GET_FILE_AUTH`;
+export const SET_FILE_AUTH = `${prefix}/SET_FILE_AUTH`;
+export const UPDATE_FILE_AUTH = `${prefix}/UPDATE_FILE_AUTH`;
+
+export const GET_DOWNLOAD_LINK = `${prefix}/GET_DOWNLOAD_LINK`;
+export const SET_DOWNLOAD_LINK = `${prefix}/SET_DOWNLOAD_LINK`;
+
+export const GET_SHARE_USER_LIST = `${prefix}/GET_SHARE_USER_LIST`;
+export const SET_SHARE_USER_LIST = `${prefix}/SET_SHARE_USER_LIST`;
+export const GET_SHARE_FILE_LIST = `${prefix}/GET_SHARE_FILE_LIST`;
+export const SET_SHARE_FILE_LIST = `${prefix}/SET_SHARE_FILE_LIST`;
