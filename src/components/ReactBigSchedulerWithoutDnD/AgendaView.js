49daffad3c04fb8eb5de7c4073f44762d20a9e56
jhjeong <jhjeong@kb-sys.co.kr>
2020-07-27 22:48:20 +0900

    [Bizmicro-portal-dev] fix - test Scheduler without dnd

---
+++ src/components/ReactBigSchedulerWithoutDnD/AgendaView.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import AgendaResourceEvents from './AgendaResourceEvents';
+
+class AgendaView extends Component {
+  static propTypes = {
+    schedulerData: PropTypes.object.isRequired,
+    subtitleGetter: PropTypes.func,
+    eventItemClick: PropTypes.func,
+    viewEventClick: PropTypes.func,
+    viewEventText: PropTypes.string,
+    viewEvent2Click: PropTypes.func,
+    viewEvent2Text: PropTypes.string,
+    slotClickedFunc: PropTypes.func,
+  };
+
+  render() {
+    const { schedulerData } = this.props;
+    const { config } = schedulerData;
+    const { renderData } = schedulerData;
+    const agendaResourceTableWidth = schedulerData.getResourceTableWidth();
+    const tableHeaderHeight = schedulerData.getTableHeaderHeight();
+    const resourceEventsList = renderData.map(item => <AgendaResourceEvents {...this.props} resourceEvents={item} key={item.slotId} />);
+    const resourceName = schedulerData.isEventPerspective ? config.taskName : config.resourceName;
+    const { agendaViewHeader } = config;
+
+    return (
+      <tr>
+        <td>
+          <table className="scheduler-table">
+            <thead>
+              <tr style={{ height: tableHeaderHeight }}>
+                <th style={{ width: agendaResourceTableWidth }} className="header3-text">
+                  {resourceName}
+                </th>
+                <th className="header3-text">{agendaViewHeader}</th>
+              </tr>
+            </thead>
+            <tbody>{resourceEventsList}</tbody>
+          </table>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default AgendaView;
