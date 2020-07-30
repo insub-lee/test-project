dd23541ac13cfa462639d3f0b7098af85fd26699
lesm74 <sangmin.biz@gmail.com>
2020-07-30 12:07:41 +0900

    포털 메인화면 수정

--- src/apps/widget/mdcs/StandardDocDashBoard/DocDashModal.js
+++ src/apps/widget/mdcs/StandardDocDashBoard/DocDashModal.js
@@ -1,13 +1,59 @@
 import React from 'react';
 import { Modal } from 'antd';
-
+import moment from 'moment';
 import StyledModalWrapper from 'apps/mdcs/styled/Modals/StyledModalWrapper';
-
+import ExcelDownLoad from 'components/ExcelDownLoad';
 import ModalView from './ModalView';
 import ModalList from './ModalList';
 import CoverView from './CoverView';
 
 const AntdModal = StyledModalWrapper(Modal);
+const excelColumns = [
+  {
+    title: '종류',
+    width: { wpx: 100 },
+    style: { alignment: { horizontal: 'center' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+  {
+    title: 'NO',
+    width: { wpx: 100 },
+    style: { alignment: { horizontal: 'center' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+  {
+    title: 'REV',
+    width: { wpx: 30 },
+    style: { alignment: { horizontal: 'center' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+  {
+    title: 'Effect Date',
+    width: { wpx: 100 },
+    style: { alignment: { horizontal: 'center' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+  {
+    title: 'Title',
+    width: { wpx: 300 },
+    style: { alignment: { horizontal: 'left' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+  {
+    title: '기안부서',
+    width: { wpx: 100 },
+    style: { alignment: { horizontal: 'center' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+  {
+    title: '기안자',
+    width: { wpx: 100 },
+    style: { alignment: { horizontal: 'center' }, font: { sz: '10' }, fill: { patternType: 'solid', fgColor: { rgb: 'CCCCCC' } } },
+  },
+];
+const fields = [
+  { field: 'FULLPATHSTR', style: { alignment: { horizontal: 'center' }, font: { sz: '10' } } },
+  { field: 'DOCNUMBER', style: { alignment: { horizontal: 'center' }, font: { sz: '10' } } },
+  { field: 'VERSION', style: { alignment: { horizontal: 'center' }, font: { sz: '10' } } },
+  { field: 'END_DTTM', style: { alignment: { horizontal: 'center' }, font: { sz: '10' } } },
+  { field: 'TITLE', style: { alignment: { horizontal: 'left' }, font: { sz: '10' } } },
+  { field: 'REG_DEPT_NAME', style: { alignment: { horizontal: 'left' }, font: { sz: '10' } } },
+  { field: 'REG_USER_NAME', style: { alignment: { horizontal: 'center' }, font: { sz: '10' } } },
+];
 
 const DocDashModal = ({
   viewType,
@@ -25,6 +71,7 @@ const DocDashModal = ({
   closeListBtnFunc,
 }) => (
   <>
+    {console.debug('listData', listData)};
     <AntdModal className="modalWrapper modalTechDoc modalCustom" visible={visible} footer={null} width={800} onCancel={closeBtnFunc} destroyOnClose>
       <>
         <div className="pop_tit">{widgetTitle}</div>
@@ -50,7 +97,22 @@ const DocDashModal = ({
     <AntdModal className="modalWrapper modalTechDoc modalCustom" visible={listVisible} footer={null} width={1000} onCancel={closeListBtnFunc} destroyOnClose>
       <>
         <div className="pop_tit">{widgetTitle}</div>
-        <div className="pop_con">
+        <div className="pop_con_pad20">
+          <div style={{ width: '100%', textAlign: 'right', marginBottom: '10px' }}>
+            <ExcelDownLoad
+              key="excelDownLoad"
+              isBuilder={false}
+              fileName={`검색결과 (${moment().format('YYYYMMDD')})`}
+              className="workerExcelBtn"
+              title="Excel 파일로 저장"
+              btnSize="btn-sm"
+              sheetName=""
+              columns={excelColumns}
+              fields={fields}
+              submitInfo={false}
+              dataSetBind={listData.map(f => ({ ...f, VERSION: f.VERSION.split('.')[0] }))}
+            />
+          </div>
           <ModalList onClickRow={onClickRow} closeBtnFunc={closeListBtnFunc} listData={listData} />
         </div>
       </>
--- src/apps/widget/mdcs/StandardDocDashBoard/ModalList.js
+++ src/apps/widget/mdcs/StandardDocDashBoard/ModalList.js
@@ -3,14 +3,10 @@ import { Table } from 'antd';
 
 import StyledButton from 'commonStyled/Buttons/StyledButton';
 import StyledLineTable from 'commonStyled/MdcsStyled/Table/StyledLineTable';
-
-const AntdLineTable = StyledLineTable(Table);
+import StyledAntdTable from 'components/BizBuilder/styled/Table/StyledAntdTable';
+const AntdLineTable = StyledAntdTable(Table);
 
 const columns = [
-  { title: 'No.', key: 'DOCNUMBER', width: '11%', dataIndex: 'DOCNUMBER' },
-  { title: 'REV.', key: 'VERSION', align: 'center', width: '7%', dataIndex: 'VERSION' },
-  { title: 'Effect Date', align: 'center', key: 'END_DTTM', width: '10%', dataIndex: 'END_DTTM' },
-  { title: 'Title', align: 'left', key: 'TITLE', dataIndex: 'TITLE' },
   {
     title: '종류',
     key: 'FULLPATHSTR',
@@ -22,7 +18,12 @@ const columns = [
       }
     },
   },
-  { title: '기안부서', key: 'REG_DEPT_NAME', width: '12%', dataIndex: 'REG_DEPT_NAME' },
+  { title: 'No.', key: 'DOCNUMBER', width: '11%', dataIndex: 'DOCNUMBER' },
+  { title: 'REV.', key: 'VERSION', align: 'center', width: '7%', dataIndex: 'VERSION', render: (text, record) => text.split('.')[0] },
+  { title: 'Effect Date', align: 'center', key: 'END_DTTM', width: '10%', dataIndex: 'END_DTTM' },
+  { title: 'Title', align: 'left', key: 'TITLE', dataIndex: 'TITLE' },
+
+  { title: '기안부서', key: 'REG_DEPT_NAME', width: '14%', dataIndex: 'REG_DEPT_NAME' },
   { title: '기안자', key: 'REG_USER_NAME', width: '8%', dataIndex: 'REG_USER_NAME' },
 ];
 
