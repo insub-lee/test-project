eef3942ea6b145edf818cfdf818592c7a9feb8d5
lovysunny <lovysunny7@daum.net>
2020-10-14 14:41:57 +0900

    resolve conflict

--- src/apps/tpms/page/Notice/index.js
+++ src/apps/tpms/page/Notice/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from 'rc-table';
 import moment from 'moment';
+import { Modal, Button } from 'antd';
 
 import GlobalStyle from '../../components/GlobalStyle';
 import TitleContainer from '../../components/TitleContainer';
@@ -12,13 +13,8 @@ import StyledBodyRow from '../../components/Tableboard/StyledBodyRow';
 import StyledHeader from '../../components/Tableboard/StyledHeader';
 import StyledHeaderCell from '../../components/Tableboard/StyledHeaderCell';
 import StyledTable from '../../components/Tableboard/StyledTable';
+import { ModalRegister, Title } from '../../components/ModalRegister';
 
-/**
- * TPMS - 공지사항
- *
- * @returns {*}
- * @constructor
- */
 const nav = [{ title: 'TPMS' }, { title: '공지사항' }];
 const categories = [
   { value: 'all', text: '전체' },
@@ -105,42 +101,75 @@ const columns = [
   },
 ];
 
-const Notice = () => (
-  <div className="tpms-view">
-    <TitleContainer title="공지사항" nav={nav}>
-      <StyledWrapper>
-        <div className="view_top">
-          <StyledSearch>
-            <form
-              autoComplete="off"
-              className="page"
-              name="form-name"
-              onSubmit={e => {
-                e.stopPropagation();
-                e.preventDefault();
-              }}
-            >
-              <select name="category">
-                {categories.map(category => (
-                  <option key={category.text} value={category.value}>
-                    {category.text}
-                  </option>
-                ))}
-              </select>
-              <input type="text" className="input" name="text" />
-              <button type="submit" className="icon icon_search_white">
-                검색
-              </button>
-            </form>
-          </StyledSearch>
-          <div className="btn_wrap" />
-        </div>
-        <Table columns={columns} rowKey="postno" rowClassName={(_record, index) => (index % 2 === 0 ? 'old' : 'even')} components={componentsStyle} />
-        <Pagination {...pagination} groupSize={10} pageHandler={() => {}} pageSizeHandler={() => {}} />
-      </StyledWrapper>
-    </TitleContainer>
-    <GlobalStyle />
-  </div>
-);
+/**
+ * TPMS - 공지사항
+ *
+ * 모달 4개
+ * 수정, 삭제, 등록, 게시글 850
+ *
+ * @returns {*}
+ * @constructor
+ */
+const Notice = () => {
+  const [openRegModal, setOpenRegModal] = useState(true);
+  // const modalWidth = window.visualViewport.width * 0.8;
+  return (
+    <div className="tpms-view">
+      <TitleContainer title="공지사항" nav={nav}>
+        <StyledWrapper>
+          <div className="view_top">
+            <StyledSearch>
+              <form
+                autoComplete="off"
+                className="page"
+                name="form-name"
+                onSubmit={e => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                }}
+              >
+                <select name="category">
+                  {categories.map(category => (
+                    <option key={category.text} value={category.value}>
+                      {category.text}
+                    </option>
+                  ))}
+                </select>
+                <input type="text" className="input" name="text" />
+                <button type="submit" className="icon icon_search_white">
+                  검색
+                </button>
+                <button type="button" onClick={() => setOpenRegModal(true)}>
+                  등록
+                </button>
+              </form>
+            </StyledSearch>
+            <div className="btn_wrap" />
+          </div>
+          <button type="button">수정</button>
+          <button type="button">삭제</button>
+          <button type="button">게시글</button>
+          <button type="button" onClick={() => setOpenRegModal(true)}>
+            등록
+          </button>
+          <Table columns={columns} rowKey="postno" rowClassName={(_record, index) => (index % 2 === 0 ? 'old' : 'even')} components={componentsStyle} />
+          <Pagination {...pagination} groupSize={10} pageHandler={() => {}} pageSizeHandler={() => {}} />
+          <Modal
+            width={1024}
+            footer={null}
+            title={<Title title="등록하기" />}
+            visible={openRegModal}
+            onCancel={() => setOpenRegModal(false)}
+            centered
+            destroyOnClose
+          >
+            <ModalRegister closeModal={setOpenRegModal} />
+          </Modal>
+        </StyledWrapper>
+      </TitleContainer>
+      <GlobalStyle />
+    </div>
+  );
+};
 
 export default Notice;
