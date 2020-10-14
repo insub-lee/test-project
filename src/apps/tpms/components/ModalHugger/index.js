5ea0fbc19090ac3ce829007fd9a4cea5c7050d15
lovysunny <lovysunny7@daum.net>
2020-10-14 16:54:39 +0900

    imprv tpms modal

--- src/apps/tpms/components/ModalHugger/index.js
+++ src/apps/tpms/components/ModalHugger/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Wrapper } from './Wrapper';
+import { Modal } from 'antd';
 
-export const ModalHugger = ({ node }) => (
+import { ModalWrapper, Wrapper } from './Wrapper';
+
+const StyledModal = ModalWrapper(Modal);
+
+export const ModalHugger = ({ closable, maskClosable, modalClassName, visible, onCancel, width, footer, title, children }) => (
   <Wrapper>
-    <div className="content">{node}</div>
+    <StyledModal
+      className={modalClassName}
+      width={width}
+      footer={footer}
+      closable={closable}
+      title={title}
+      visible={visible}
+      onCancel={() => onCancel()}
+      centered
+      destroyOnClose
+      maskClosable={maskClosable}
+    >
+      {children}
+    </StyledModal>
   </Wrapper>
 );
 
 ModalHugger.propTypes = {
-  node: PropTypes.node,
+  modalClassName: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  visible: PropTypes.bool,
+  maskClosable: PropTypes.bool,
+  closable: PropTypes.bool,
+  onCancel: PropTypes.func,
+  title: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  footer: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  children: PropTypes.oneOfType([PropTypes.element]),
 };
 
-ModalHugger.defaultProps = { node: null };
+ModalHugger.defaultProps = {
+  maskClosable: true,
+  modalClassName: '',
+  width: '',
+  visible: false,
+  closable: true,
+  onCancel: () => {},
+  title: '',
+  footer: '',
+  children: null,
+};
