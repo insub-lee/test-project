5a4da70c40b30b70460b2f91adc4dba813f16312
jhjeong <jhjeong@kb-sys.co.kr>
2020-10-22 15:31:26 +0900

    Add - 메인페이지 Dashboard 추가 및 일부 게시판 셀렉트 스타일 수정

--- src/apps/tpms/page/Main/useHooks.js
+++ src/apps/tpms/page/Main/useHooks.js
@@ -8,6 +8,12 @@ export default () => {
         comtype: '01',
         cont:
           '[{"type":"text","option":{"label":"제목","name":"title","placeholder":"제목을 입력해주세요.","value":"","required":true},"seq":0},{"type":"richTextEditor","option":{"label":"내용","name":"textarea-1539755684624","value":"","required":true},"seq":1},{"type":"password","option":{"label":"비밀번호","name":"pwd","placeholder":"비밀번호를 입력해주세요.","value":"","required":false},"seq":2},{"type":"uploader","option":{"label":"파일첨부","name":"uploader-attach"},"seq":3}]',
+        formJson: [
+          { type: 'text', option: { label: '제목', name: 'title', placeholder: '제목을 입력해주세요.', value: '', required: true }, seq: 0 },
+          { type: 'richTextEditor', option: { label: '내용', name: 'textarea-1539755684624', value: '', required: true }, seq: 1 },
+          { type: 'password', option: { label: '비밀번호', name: 'pwd', placeholder: '비밀번호를 입력해주세요.', value: '', required: false }, seq: 2 },
+          { type: 'uploader', option: { label: '파일첨부', name: 'uploader-attach' }, seq: 3 },
+        ],
         id: 'brd00000000000000002',
         itemid: 'item00000000000000000000000018',
         itemtype: '00',
@@ -24,6 +30,11 @@ export default () => {
         comtype: '01',
         cont:
           '[{"type":"text","option":{"label":"제목","name":"title","placeholder":"제목을 입력해주세요.","value":"","required":true},"seq":0},{"type":"richTextEditor","option":{"label":"내용","name":"textarea-1539755684624","value":"","required":true},"seq":1},{"type":"password","option":{"label":"비밀번호","name":"pwd","placeholder":"비밀번호를 입력해주세요.","value":"","required":false},"seq":2}]',
+        formJson: [
+          { type: 'text', option: { label: '제목', name: 'title', placeholder: '제목을 입력해주세요.', value: '', required: true }, seq: 0 },
+          { type: 'richTextEditor', option: { label: '내용', name: 'textarea-1539755684624', value: '', required: true }, seq: 1 },
+          { type: 'password', option: { label: '비밀번호', name: 'pwd', placeholder: '비밀번호를 입력해주세요.', value: '', required: false }, seq: 2 },
+        ],
         id: 'brd00000000000000005',
         itemid: 'item00000000000000000000000019',
         itemtype: '00',
@@ -40,6 +51,29 @@ export default () => {
         comtype: '01',
         cont:
           '[{"type":"text","option":{"label":"제목","name":"title","placeholder":"제목을 입력하세요.","value":"","required":true},"seq":0},{"type":"select","option":{"label":"년도","name":"year","values":[{"id":1579510209014,"seq":2,"label":"2020","value":"2020","checked":true},{"id":1552006606918,"seq":0,"label":"2019","value":"2019","checked":false},{"id":1552006621940,"seq":1,"label":"2018","value":"2018","checked":false}]},"seq":1},{"type":"richTextEditor","option":{"label":"내용","name":"textarea-1542189210177","placeholder":"내용을 입력하세요.","value":"","required":true},"seq":2},{"type":"password","option":{"label":"비밀번호","name":"pwd","placeholder":"비밀번호를 입력해주세요.","value":"","required":false},"seq":3},{"type":"uploader","option":{"label":"파일첨부","name":"uploader-attach"},"seq":4}]',
+        formJson: [
+          { type: 'text', option: { label: '제목', name: 'title', placeholder: '제목을 입력하세요.', value: '', required: true }, seq: 0 },
+          {
+            type: 'select',
+            option: {
+              label: '년도',
+              name: 'year',
+              values: [
+                { id: 1579510209014, seq: 2, label: '2020', value: '2020', checked: true },
+                { id: 1552006606918, seq: 0, label: '2019', value: '2019', checked: false },
+                { id: 1552006621940, seq: 1, label: '2018', value: '2018', checked: false },
+              ],
+            },
+            seq: 1,
+          },
+          {
+            type: 'richTextEditor',
+            option: { label: '내용', name: 'textarea-1542189210177', placeholder: '내용을 입력하세요.', value: '', required: true },
+            seq: 2,
+          },
+          { type: 'password', option: { label: '비밀번호', name: 'pwd', placeholder: '비밀번호를 입력해주세요.', value: '', required: false }, seq: 3 },
+          { type: 'uploader', option: { label: '파일첨부', name: 'uploader-attach' }, seq: 4 },
+        ],
         id: 'brd00000000000000007',
         itemid: 'item00000000000000000000000026',
         itemtype: '00',
