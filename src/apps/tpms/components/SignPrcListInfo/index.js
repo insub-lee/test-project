2d77de17d2ff2cea9c769f96a54dfb03bb29283a
loganleee <dlfjgkssk1@gmail.com>
2020-10-26 11:51:19 +0900

    add signPrcListInfo

---
+++ src/apps/tpms/components/SignPrcListInfo/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import Styled from './Styled';
+
+const getDateString = (value, format = 'YYYY-MM-DD') => (value ? moment(value, 'YYYYMMDD').format(format) : '');
+
+const SignPrcListInfo = ({ list, noPadding }) => (
+  <Styled noPadding={noPadding}>
+    <table>
+      <colgroup>
+        <col width="calc(100% / 7)" />
+        <col width="calc(100% / 7)" />
+        <col width="calc(100% / 7)" />
+        <col width="calc(100% / 7)" />
+        <col width="calc(100% / 7)" />
+        <col width="calc(100% / 7)" />
+        <col width="calc(100% / 7)" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th>결재 진행단계</th>
+          <th>사번</th>
+          <th>부서</th>
+          <th>직급</th>
+          <th>성명</th>
+          <th>완료일</th>
+          <th>상태</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.length === 0 && (
+          <tr>
+            <td colSpan="7">진행된 내용이 없습니다.</td>
+          </tr>
+        )}
+        {list.map(row => {
+          if (row.step === 'Drop_사유') {
+            return (
+              <tr key={row.seq}>
+                <td>{row.step}</td>
+                <td colSpan={6}>{row.dropreason}</td>
+              </tr>
+            )
+          }
+          return (
+            <tr key={row.seq}>
+              <td>{row.step}</td>
+              <td>{row.usrid}</td>
+              <td>{row.dpnm}</td>
+              <td>{row.jgnm}</td>
+              <td>{row.usrnm}</td>
+              <td>{getDateString(row.approvedt)}</td>
+              <td>{row.sign}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </Styled>
+);
+
+SignPrcListInfo.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object),
+  noPadding: PropTypes.bool,
+};
+
+SignPrcListInfo.defaultProps = {
+  list: [],
+  noPadding: false,
+};
+
+export default SignPrcListInfo;
