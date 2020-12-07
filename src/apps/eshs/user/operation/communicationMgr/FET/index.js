9e1279bc11d0c8944d3280c8b2279745e5dca5fd
Hyune <hyune7142@kb-sys.co.kr>
2020-12-07 16:12:17 +0900

    빌더 모달팝업 뷰Meta 지정할수 있도록 props 추가

--- src/apps/eshs/user/operation/communicationMgr/FET/index.js
+++ src/apps/eshs/user/operation/communicationMgr/FET/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import CommunicationMgr from '../pages';
 
 // 의사소통관리 - 설비기술팀
-const CommunicationMgrPage = () => <CommunicationMgr pageType="FET" />;
+const CommunicationMgrPage = () => <CommunicationMgr pageType="FET" inputMetaSeq={16801} />;
 
 export default CommunicationMgrPage;
