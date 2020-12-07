748f3e2b92ae3a52641d40b5f4f809966143fc26
Hyune <hyune7142@kb-sys.co.kr>
2020-12-07 14:24:04 +0900

    ESH - 경영 페이지 재개발

--- src/apps/eshs/user/operation/eshBusinessSystem/goalManagement/index.js
+++ src/apps/eshs/user/operation/eshBusinessSystem/goalManagement/index.js
@@ -1,29 +1,6 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
+import React from 'react';
 import BizBuilderBase from 'components/BizBuilderBase';
-import List from 'apps/eshs/user/pages/ListPage';
 
-class GoalManagement extends Component {
-  state = {
-    isLoading: true,
-  };
-
-  componentDidMount() {}
-
-  loadingComplete = () => {
-    this.setState({
-      isLoading: false,
-    });
-  };
-
-  render() {
-    return <BizBuilderBase sagaKey="GoalManagement" workSeq={1841} viewType="LIST" loadingComplete={this.loadingComplete} />;
-  }
-}
-
-GoalManagement.propTypes = {};
-
-GoalManagement.defaultProps = {};
+const GoalManagement = () => <BizBuilderBase sagaKey="GoalManagement" workSeq={1841} viewType="LIST" />;
 
 export default GoalManagement;
